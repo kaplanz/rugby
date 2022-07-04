497397f15c52e9a5ebd008e4da994802f798d6f1
Zakhary Kaplan <zakharykaplan@gmail.com>
2022-07-04 00:00:00 -0400

    refactor(dmg): upload `model` to core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,10 +4,12 @@
 //! components of the Nintendo Game Boy family of consoles.
 
 pub use crate::emu::Emulator;
+pub use crate::model::dmg::GameBoy;
 
 pub mod dev;
 mod emu;
 pub mod hw;
 pub mod io;
+pub mod model;
 pub mod spec;
 pub mod util;
