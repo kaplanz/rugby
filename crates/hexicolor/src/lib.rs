261e6bee3f7f2398b0377ddc8ec5037332ee6e9c
Zakhary Kaplan <zakharykaplan@gmail.com>
2022-07-05 00:00:00 -0400

    feat(hexicolor): initial

---
+++ crates/hexicolor/src/lib.rs
@@ -0,0 +1,108 @@
+//! Hex color parser.
+//!
+//! This library implements a string color parser for hexadecimal color values.
+
+use std::fmt::Display;
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+use thiserror::Error;
+
+/// 32-bit color.
+#[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Color(u32);
+
+impl Color {
+    pub fn new(color: u32) -> Self {
+        Self(color)
+    }
+
+    pub fn rgb(r: u8, g: u8, b: u8) -> Self {
+        Self((r as u32) << 16 | (g as u32) << 8 | (b as u32))
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&format!("#{:06x}", self.0), f)
+    }
+}
+
+impl From<Color> for u32 {
+    fn from(color: Color) -> Self {
+        color.0
+    }
+}
+
+impl FromStr for Color {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.chars().next().ok_or(Error::Empty)? {
+            '#' => {
+                let input = s.get(1..).ok_or(Error::Unsupported)?;
+                let hex = u32::from_str_radix(input, 16).map_err(Error::ParseInt)?;
+                match input.len() {
+                    3 => Ok(Color(hex << 12 | hex)),
+                    6 => Ok(Color(hex)),
+                    _ => Err(Error::Unsupported),
+                }
+            }
+            _ => Err(Error::MissingHash),
+        }
+    }
+}
+
+/// A type specifying general categories of [`Color`] error.
+#[derive(Clone, Debug, Error)]
+pub enum Error {
+    #[error("could not parse empty string")]
+    Empty,
+    #[error("must start with \"#\"")]
+    MissingHash,
+    #[error("could not parse hex")]
+    ParseInt(#[from] ParseIntError),
+    #[error("unsupported color format")]
+    Unsupported,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_works() {
+        assert_eq!("#123".parse::<Color>().unwrap(), Color(0x123123));
+        assert_eq!("#123456".parse::<Color>().unwrap(), Color(0x123456));
+        assert_eq!("#AbCdEf".parse::<Color>().unwrap(), Color(0xabcdef));
+    }
+
+    #[test]
+    fn parse_errors() {
+        assert!(matches!("".parse::<Color>().unwrap_err(), Error::Empty));
+        assert!(matches!(
+            "shalom".parse::<Color>().unwrap_err(),
+            Error::MissingHash
+        ));
+        assert!(matches!(
+            "#".parse::<Color>().unwrap_err(),
+            Error::ParseInt(_)
+        ));
+        assert!(matches!(
+            "#shalom".parse::<Color>().unwrap_err(),
+            Error::ParseInt(_)
+        ));
+        assert!(matches!(
+            "#12".parse::<Color>().unwrap_err(),
+            Error::Unsupported
+        ));
+        assert!(matches!(
+            "#1234".parse::<Color>().unwrap_err(),
+            Error::Unsupported
+        ));
+        assert!(matches!(
+            "#1234567".parse::<Color>().unwrap_err(),
+            Error::Unsupported
+        ));
+    }
+}
--- src/palette.rs
+++ src/palette.rs
@@ -4,34 +4,72 @@ use std::fmt::{Debug, Display};
 use std::ops::Index;
 use std::str::FromStr;
 
-use serde::{Deserialize as De, Serialize as Ser};
+use thiserror::Error;
 
 /// Use [24-bit] color (stored as `0x00RRGGBB_u32`)
 ///
 /// [24-bit]: https://en.wikipedia.org/wiki/List_of_monochrome_and_RGB_color_formats#24-bit_RGB
-type Color = u32;
+#[derive(Copy, Clone, Debug, Default)]
+pub struct Color(u32);
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&format!("#{:06x}", self.0), f)
+    }
+}
+
+impl FromStr for Color {
+    type Err = hexicolor::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Color(s.parse::<hexicolor::Color>()?.into()))
+    }
+}
+
+impl From<Color> for u32 {
+    fn from(value: Color) -> Self {
+        value.0
+    }
+}
 
 /// Color palette.
-#[derive(Debug, Ser, De)]
+#[derive(Clone, Debug)]
 pub struct Palette([Color; 4]);
 
 impl Default for Palette {
     fn default() -> Self {
-        Self([0xe9efec, 0xa0a08b, 0x555568, 0x211e20])
+        Self([
+            Color(0xe9efec),
+            Color(0xa0a08b),
+            Color(0x555568),
+            Color(0x211e20),
+        ])
     }
 }
 
 impl Display for Palette {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        Display::fmt(&json5::to_string(self).unwrap(), f)
+        Display::fmt(
+            &format!("{},{},{},{}", self.0[0], self.0[1], self.0[2], self.0[3]),
+            f,
+        )
     }
 }
 
 impl FromStr for Palette {
-    type Err = json5::Error;
+    type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        json5::from_str(s)
+        let palette: Vec<Color> = match s
+            .split(',')
+            .map(str::parse::<Color>)
+            .collect::<Result<_, _>>()
+        {
+            Ok(value) => value,
+            Err(err) => return Err(Error::Parse(err)),
+        };
+        let palette: [Color; 4] = palette.try_into().map_err(Error::PaletteSize)?;
+        Ok(Palette(palette))
     }
 }
 
@@ -41,4 +79,12 @@ impl Index<usize> for Palette {
     fn index(&self, index: usize) -> &Self::Output {
         &self.0[index]
     }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Parse(hexicolor::Error),
+    #[error("incorrect palette size")]
+    PaletteSize(Vec<Color>),
 }
