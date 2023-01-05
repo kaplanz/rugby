08700818b80d2e9285cf83320e423de14f96e4df
Zakhary Kaplan <me@zakhary.dev>
2023-01-05 18:00:00 -0500

    refactor(dmg): improve booting

--- core/src/hw/ppu/exec/vblank.rs
+++ core/src/hw/ppu/exec/vblank.rs
@@ -14,8 +14,7 @@ impl VBlank {
             Mode::VBlank(self)
         } else {
             // Extract scanline info
-            let regs = ppu.ctl.borrow();
-            let ly = &mut **regs.ly.borrow_mut();
+            let ly = &mut **ppu.ctl.ly.borrow_mut();
             // Increment scanline at the 456th dot, and reset dot-clock
             *ly += 1;
             ppu.dot = 0;
--- core/src/model/dmg/mem.rs
+++ core/src/model/dmg/mem.rs
@@ -3,32 +3,62 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+use remus::bus::adapt::View;
+use remus::bus::Bus;
 use remus::mem::Ram;
-use remus::Block;
+use remus::{Block, Device};
 
-use super::boot;
+use super::boot::Rom as BootRom;
+use super::Board;
 
-/// Input/Output connections.
+pub type Wram = Ram<0x2000>;
+pub type Hram = Ram<0x007f>;
+
+/// On-chip memory.
 ///
-/// This struct is plain old data (POD), and its members are accessed by
+/// This struct is plain old data (POD), and its members are accessible by
 /// [`GameBoy`](super::GameBoy).
 #[derive(Debug, Default)]
 pub struct Memory {
     // ┌────────┬──────┬─────┬───────┐
-    // │  SIZE  │ NAME │ DEV │ ALIAS │
+    // │  Size  │ Name │ Dev │ Alias │
     // ├────────┼──────┼─────┼───────┤
     // │  256 B │ Boot │ ROM │       │
-    // │ 8 Ki B │ Work │ RAM │ WRAM  │
+    // │  8 KiB │ Work │ RAM │ WRAM  │
     // │  127 B │ High │ RAM │ HRAM  │
     // └────────┴──────┴─────┴───────┘
-    pub boot: Rc<RefCell<boot::Rom>>,
-    pub wram: Rc<RefCell<Ram<0x2000>>>,
-    pub hram: Rc<RefCell<Ram<0x007f>>>,
+    pub(super) boot: Rc<RefCell<BootRom>>,
+    pub(super) wram: Rc<RefCell<Wram>>,
+    pub(super) hram: Rc<RefCell<Hram>>,
 }
 
 impl Block for Memory {
     fn reset(&mut self) {
-        // Reset boot ROM
         self.boot.borrow_mut().reset();
+        self.wram.borrow_mut().reset();
+        self.hram.borrow_mut().reset();
+    }
+}
+
+impl Board for Memory {
+    #[rustfmt::skip]
+    fn connect(&self, bus: &mut Bus) {
+        // Connect boards
+        self.boot.borrow().connect(bus);
+
+        // Extract devices
+        let wram = self.wram.clone();
+        let echo = View::new(wram.clone(), 0x0000..=0x1dff).to_shared();
+        let hram = self.hram.clone();
+
+        // Map devices on bus  // ┌──────┬────────┬──────┬─────┐
+                               // │ Addr │  Size  │ Name │ Dev │
+                               // ├──────┼────────┼──────┼─────┤
+        // mapped by `boot`    // │ 0000 │  256 B │ Boot │ ROM │
+        bus.map(0xc000, wram); // │ c000 │  8 KiB │ Work │ RAM │
+        bus.map(0xe000, echo); // │ e000 │ 7680 B │ Echo │ RAM │
+        // mapped by `boot`    // │ ff50 │    1 B │ Boot │ Reg │
+        bus.map(0xff80, hram); // │ ff80 │  127 B │ High │ RAM │
+                               // └──────┴────────┴──────┴─────┘
     }
 }
