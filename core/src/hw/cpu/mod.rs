4b9868ea764cfbf27a74f263c91e3b8d571f37cd
Zakhary Kaplan <me@zakhary.dev>
2023-01-06 10:30:00 -0500

    feat(boot): enable without boot ROM

--- core/src/hw/cpu/mod.rs
+++ core/src/hw/cpu/mod.rs
@@ -17,12 +17,30 @@ pub use self::sm83::Cpu as Sm83;
 
 /// Unified processor interface.
 pub trait Processor: Block + Machine {
+    /// The processor's register set.
+    type Register;
+
+    /// Gets the value of the requested register.
+    fn get(&self, reg: Self::Register) -> u16;
+
+    /// Sets the value of the requested register.
+    fn set(&mut self, reg: Self::Register, value: u16);
+
+    /// Move the PC to the provided address.
+    fn goto(&mut self, pc: u16);
+
+    /// Execute the provided instruction in-place.
+    fn exec(&mut self, opcode: u8);
+
+    /// Run the provided program (i.e. instruction sequence) in-place.
+    fn run(&mut self, prog: &[u8]);
+
+    /// Enable (or wake) the processor.
+    fn wake(&mut self);
+
     /// Sets the processor's memory bus.
     fn set_bus(&mut self, bus: Rc<RefCell<Bus>>);
 
     /// Sets the processor's interrupt controller.
     fn set_pic(&mut self, pic: Rc<RefCell<Pic>>);
-
-    /// Enable (or wake) the processor.
-    fn wake(&mut self);
 }
