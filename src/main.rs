84b81b6f021da21c94ff044447b3d212f783c735
Zakhary Kaplan <me@zakhary.dev>
2023-03-29 22:00:00 -0400

    refactor(bin): split main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
-#![allow(clippy::too_many_lines)]
-
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 
 use clap::{Parser, ValueEnum, ValueHint};
 use color_eyre::eyre::{Result, WrapErr};
-use gameboy::core::Emulator;
 use gameboy::dmg::cart::{Cartridge, Header};
-use gameboy::dmg::{BootRom, Button, GameBoy, Screen, SCREEN};
-use log::{debug, info, warn};
-use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
-use remus::{Clock, Machine};
+use gameboy::dmg::{BootRom, GameBoy, SCREEN};
+use log::{info, warn};
+use minifb::{Scale, ScaleMode, Window, WindowOptions};
 
-use crate::palette::Palette;
+use crate::app::{App, Opts};
+use crate::pal::Palette;
 
-mod palette;
+mod app;
+mod pal;
 
 /// Game Boy main clock frequency, set to 4,194,304 Hz.
 const FREQ: u32 = 0x0040_0000;
@@ -103,75 +101,14 @@ fn main() -> Result<()> {
     // Parse args
     let args = Args::parse();
 
-    // Prepare the boot ROM
-    let boot = args
-        .boot
-        .map(|boot| -> Result<_> {
-            // Open boot ROM file
-            let mut f = File::open(&boot)
-                .with_context(|| format!("failed to open boot ROM: `{}`", boot.display()))?;
-            // Read boot ROM into a buffer
-            // NOTE: Boot ROM is exactly 256 bytes.
-            let mut buf = [0u8; 0x0100];
-            f.read_exact(&mut buf)
-                .with_context(|| format!("failed to read full boot ROM: `{}`", boot.display()))?;
-            info!(
-                "Read {} bytes from boot ROM: `{}`",
-                buf.len(),
-                boot.display(),
-            );
-
-            Ok(buf)
-        })
-        .transpose()?;
-    // Initialize the boot rom
-    let boot = boot.as_ref().map(BootRom::from);
-
-    // Prepare the cartridge
-    let cart = if let Some(path) = args.rom {
-        let rom = {
-            // Open ROM file
-            let f = File::open(&path)
-                .with_context(|| format!("failed to open ROM: `{}`", path.display()))?;
-            // Read ROM into a buffer
-            // NOTE: Game Paks manufactured by Nintendo have a maximum 8 MiB ROM.
-            let mut buf = Vec::new();
-            let read = f
-                .take(0x0080_0000)
-                .read_to_end(&mut buf)
-                .with_context(|| format!("failed to read ROM: `{}`", path.display()))?;
-            info!("Read {read} bytes from ROM: `{}`", path.display());
-
-            buf
-        };
-
-        // Check ROM integrity
-        if args.chk {
-            Header::check(&rom).with_context(|| "failed ROM integrity check")?;
-            info!("Passed ROM integrity check");
-        }
-
-        // Initialize the cartridge
-        let cart = if args.force {
-            // Force cartridge from ROM
-            Cartridge::unchecked(&rom)
-        } else {
-            // Exit on cartridge failure
-            Cartridge::new(&rom)
-                .with_context(|| format!("failed to load cartridge: `{}`", path.display()))?
-        };
-        info!("Loaded cartridge:\n{}", cart.header());
-
-        cart
-    } else {
-        warn!("Missing cartridge; defaulting to blank");
-        Cartridge::blank()
-    };
-    // Extract ROM title from cartridge
+    // Read the boot ROM
+    let boot = boot(args.boot)?;
+    // Read the cartridge
+    let cart = cart(args.rom, args.chk, args.force)?;
     let title = match cart.header().title.replace('\0', " ").trim() {
         "" => "Game Boy",
         title => title,
-    }
+    } // extract title from cartridge
     .to_string();
 
     // Create emulator instance
@@ -195,145 +132,97 @@ fn main() -> Result<()> {
         scale_mode: ScaleMode::AspectRatioStretch,
         ..Default::default()
     };
-    // Create debug mode windows
-    let mut debug = if args.debug {
-        Some(Debug {
-            tdat: Window::new("Tile Data", 16 * 8, 24 * 8, opts).unwrap(),
-            map1: Window::new(
-                "Tile Map 1",
-                32 * 8,
-                32 * 8,
-                WindowOptions {
-                    scale: Scale::X1,
-                    ..opts
-                },
-            )
-            .unwrap(),
-            map2: Window::new(
-                "Tile Map 2",
-                32 * 8,
-                32 * 8,
-                WindowOptions {
-                    scale: Scale::X1,
-                    ..opts
-                },
-            )
-            .unwrap(),
-        })
+    // Create main window
+    let win = Window::new(&title, SCREEN.width, SCREEN.height, opts).unwrap();
+
+    // Create debug info
+    let debug = if args.debug {
+        Some(app::Debug::new(opts))
     } else {
         None
     };
-    // Create a framebuffer window
-    let mut win = Window::new(&title, SCREEN.width, SCREEN.height, opts).unwrap();
 
-    // Create 4 MiHz clock to sync emulator
-    let divider = 0x100; // user a clock divider to sync
-    let freq = match args.speed {
-        Speed::Half => FREQ / 2,
-        Speed::Full => FREQ,
-        Speed::Double => 2 * FREQ,
-        Speed::Triple => 3 * FREQ,
-        Speed::Max => divider, // special case
-        Speed::Custom(freq) => freq,
+    // Prepare app, options
+    let Args { pal, speed, .. } = args;
+    let opts = Opts { title, pal, speed };
+    let app = App {
+        opts,
+        emu,
+        win,
+        debug,
     };
-    let mut clk = Clock::with_freq(freq / divider);
 
-    // Initialize timer, counters
-    let mut now = std::time::Instant::now();
-    let mut cycles = 0;
-    let mut fps = 0;
-
-    // Emulation loop
-    while win.is_open() {
-        // Calculate wall-clock frequency
-        if now.elapsed().as_secs() > 0 {
-            // Print cycle stats
-            debug!(
-                "Frequency: {freq:0.4} MHz ({speedup:.1}%), FPS: {fps} Hz",
-                freq = f64::from(cycles) / 1e6,
-                speedup = 100. * f64::from(cycles) / f64::from(FREQ)
-            );
-            // Update the title to display the frequency
-            win.set_title(&format!("{title} ({fps} Hz)"));
-            // Reset timer, counters
-            now = std::time::Instant::now();
-            cycles = 0;
-            fps = 0;
-        }
-
-        // Synchronize with wall-clock
-        if cycles % divider == 0 && args.speed != Speed::Max {
-            // Delay until clock is ready
-            clk.next();
-        }
-
-        // Perform a single cycle
-        emu.cycle();
-
-        // Redraw the screen (if needed)
-        emu.redraw(|screen: &Screen| {
-            let buf = screen
-                .iter()
-                .map(|&col| args.pal[col as usize].into())
-                .collect::<Vec<_>>();
-            win.update_with_buffer(&buf, SCREEN.width, SCREEN.height)
-                .unwrap();
-            fps += 1; // update frames drawn
-        });
-
-        // Update the debug screens every second
-        if let Some(debug) = &mut debug {
-            if cycles == 0 {
-                // Probe for debug info
-                let info = emu.debug();
-
-                // Extract PPU state
-                let tdat = info.ppu.tdat.map(|col| args.pal[col as usize].into());
-                let map1 = info.ppu.map1.map(|col| args.pal[col as usize].into());
-                let map2 = info.ppu.map2.map(|col| args.pal[col as usize].into());
-                // Display PPU state
-                debug
-                    .tdat
-                    .update_with_buffer(&tdat, 16 * 8, 24 * 8)
-                    .unwrap();
-                debug
-                    .map1
-                    .update_with_buffer(&map1, 32 * 8, 32 * 8)
-                    .unwrap();
-                debug
-                    .map2
-                    .update_with_buffer(&map2, 32 * 8, 32 * 8)
-                    .unwrap();
-            }
-        }
-
-        // Send joypad input (sampled every 64 cycles)
-        if cycles % 0x40 == 0 {
-            #[rustfmt::skip]
-            let keys = win.get_keys().into_iter().filter_map(|key| match key {
-                Key::Z     => Some(Button::A),
-                Key::X     => Some(Button::B),
-                Key::Space => Some(Button::Select),
-                Key::Enter => Some(Button::Start),
-                Key::Right => Some(Button::Right),
-                Key::Left  => Some(Button::Left),
-                Key::Up    => Some(Button::Up),
-                Key::Down  => Some(Button::Down),
-                _ => None
-            }).collect::<Vec<_>>();
-            emu.send(&keys);
-        }
-
-        // Clock another cycle
-        cycles += 1;
-    }
+    // Run the app
+    app.run();
 
     Ok(())
 }
 
-#[derive(Debug)]
-struct Debug {
-    tdat: Window,
-    map1: Window,
-    map2: Window,
+fn boot(path: Option<PathBuf>) -> Result<Option<BootRom>> {
+    // Prepare the boot ROM
+    let boot = path
+        .map(|boot| -> Result<_> {
+            // Open boot ROM file
+            let mut f = File::open(&boot)
+                .with_context(|| format!("failed to open boot ROM: `{}`", boot.display()))?;
+            // Read boot ROM into a buffer
+            // NOTE: Boot ROM is exactly 256 bytes.
+            let mut buf = [0u8; 0x0100];
+            f.read_exact(&mut buf)
+                .with_context(|| format!("failed to read full boot ROM: `{}`", boot.display()))?;
+            info!(
+                "Read {} bytes from boot ROM: `{}`",
+                buf.len(),
+                boot.display(),
+            );
+
+            Ok(buf)
+        })
+        .transpose()?;
+    // Initialize the boot rom
+    let boot = boot.as_ref().map(BootRom::from);
+
+    Ok(boot)
+}
+
+fn cart(path: Option<PathBuf>, chk: bool, force: bool) -> Result<Cartridge> {
+    if let Some(path) = path {
+        let rom = {
+            // Open ROM file
+            let f = File::open(&path)
+                .with_context(|| format!("failed to open ROM: `{}`", path.display()))?;
+            // Read ROM into a buffer
+            // NOTE: Game Paks manufactured by Nintendo have a maximum 8 MiB ROM.
+            let mut buf = Vec::new();
+            let read = f
+                .take(0x0080_0000)
+                .read_to_end(&mut buf)
+                .with_context(|| format!("failed to read ROM: `{}`", path.display()))?;
+            info!("Read {read} bytes from ROM: `{}`", path.display());
+
+            buf
+        };
+
+        // Check ROM integrity
+        if chk {
+            Header::check(&rom).with_context(|| "failed ROM integrity check")?;
+            info!("Passed ROM integrity check");
+        }
+
+        // Initialize the cartridge
+        let cart = if force {
+            // Force cartridge from ROM
+            Cartridge::unchecked(&rom)
+        } else {
+            // Exit on cartridge failure
+            Cartridge::new(&rom)
+                .with_context(|| format!("failed to load cartridge: `{}`", path.display()))?
+        };
+        info!("Loaded cartridge:\n{}", cart.header());
+
+        Ok(cart)
+    } else {
+        warn!("Missing cartridge; defaulting to blank");
+        Ok(Cartridge::blank())
+    }
 }
