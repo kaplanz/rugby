4a30c908372cc2aad362d143c632b7391b4d3264
Zakhary Kaplan <me@zakhary.dev>
2023-07-26 00:00:00 -0400

    refactor(core): remove screen interface

--- core/src/emu/screen.rs
+++ core/src/emu/screen.rs
@@ -1,7 +1,6 @@
 //! Screen interface.
 
 use std::fmt::Debug;
-use std::ops::DerefMut;
 
 /// Screen info.
 #[derive(Debug)]
@@ -14,13 +13,4 @@ impl Info {
     pub const fn depth(&self) -> usize {
         self.width.saturating_mul(self.height)
     }
-}
-
-/// Screen interface.
-pub trait Screen: Clone + Debug + DerefMut<Target = [Self::Pixel]> {
-    /// Individual pixel.
-    type Pixel;
-
-    /// Redraws the screen using the provided `callback` function.
-    fn redraw(&self);
 }
