de1218723bcbad1df169a269e478fe28992117b4
Zakhary Kaplan <me@zakhary.dev>
2023-09-11 00:00:00 -0400

    fix(cpu): improve {CALL, JP, JR, RET, RETI, RST} accuracy

--- core/src/hw/cpu/sm83/insn/exec/call.rs
+++ core/src/hw/cpu/sm83/insn/exec/call.rs
@@ -4,29 +4,30 @@ use remus::Cell;
 use super::{Cpu, Error, Execute, Flag, Operation, Return};
 
 pub const fn default() -> Operation {
-    Operation::Call(Call::Fetch)
+    Operation::Call(Call::Fetch0)
 }
 
 #[derive(Clone, Debug, Default)]
 pub enum Call {
     #[default]
-    Fetch,
-    Evaluate(u16),
-    Check(u16, bool),
-    Push(u16),
-    Delay(u16),
+    Fetch0,
+    Fetch1(u8),
+    Check(u16),
+    Push0(u16),
+    Push1(u16, u8),
     Jump(u16),
 }
 
 impl Execute for Call {
+    #[rustfmt::skip]
     fn exec(self, code: u8, cpu: &mut Cpu) -> Return {
         match self {
-            Self::Fetch => fetch(code, cpu),
-            Self::Evaluate(a16) => evaluate(code, cpu, a16),
-            Self::Check(a16, cond) => check(code, cpu, a16, cond),
-            Self::Push(a16) => push(code, cpu, a16),
-            Self::Delay(a16) => delay(code, cpu, a16),
-            Self::Jump(a16) => jump(code, cpu, a16),
+            Self::Fetch0          => fetch0(code, cpu),
+            Self::Fetch1(a8)      => fetch1(code, cpu, a8),
+            Self::Check(a16)      => check(code, cpu, a16),
+            Self::Push0(a16)      => push0(code, cpu, a16),
+            Self::Push1(a16, pc0) => push1(code, cpu, a16, pc0),
+            Self::Jump(a16)       => jump(code, cpu, a16),
         }
     }
 }
@@ -37,59 +38,65 @@ impl From<Call> for Operation {
     }
 }
 
-fn fetch(code: u8, cpu: &mut Cpu) -> Return {
+fn fetch0(code: u8, cpu: &mut Cpu) -> Return {
     // Check opcode
     match code {
         0xc4 | 0xcc | 0xcd | 0xd4 | 0xdc => {
-            // Fetch u16
-            let a16 = cpu.fetchword();
+            // Fetch lower(a16) <- [PC]
+            let a8 = cpu.fetchbyte();
             // Proceed
-            Ok(Some(Call::Evaluate(a16).into()))
+            Ok(Some(Call::Fetch1(a8).into()))
         }
         code => Err(Error::Opcode(code)),
     }
 }
 
-fn evaluate(code: u8, cpu: &mut Cpu, a16: u16) -> Return {
-    // Delay by 1 cycle
+fn fetch1(_: u8, cpu: &mut Cpu, a8: u8) -> Return {
+    // Fetch upper(a16) <- [PC + 1]
+    let b8 = cpu.fetchbyte();
+    // Combine into a16
+    let a16 = u16::from_le_bytes([a8, b8]);
 
+    // Proceed
+    Ok(Some(Call::Check(a16).into()))
+}
+
+fn check(code: u8, cpu: &mut Cpu, a16: u16) -> Return {
     // Evaluate condition
     let flags = &cpu.file.f.load();
+    #[rustfmt::skip]
     let cond = match code {
         0xc4 => !Flag::Z.get(flags),
-        0xcc => Flag::Z.get(flags),
-        0xcd => true,
+        0xcc =>  Flag::Z.get(flags),
         0xd4 => !Flag::C.get(flags),
-        0xdc => Flag::C.get(flags),
+        0xdc =>  Flag::C.get(flags),
+        0xcd => true,
         code => return Err(Error::Opcode(code)),
     };
 
-    // Proceed
-    Ok(Some(Call::Check(a16, cond).into()))
-}
-
-fn check(_: u8, _: &mut Cpu, a16: u16, cond: bool) -> Return {
-    // Execute CALL
+    // Check condition
     if cond {
         // Proceed
-        Ok(Some(Call::Push(a16).into()))
+        Ok(Some(Call::Push0(a16).into()))
     } else {
         // Finish
         Ok(None)
     }
 }
 
-fn push(_: u8, cpu: &mut Cpu, a16: u16) -> Return {
-    // Push [SP] <- PC
-    let pc = cpu.file.pc.load();
-    cpu.pushword(pc);
+fn push0(_: u8, cpu: &mut Cpu, a16: u16) -> Return {
+    // Load PC
+    let pc = cpu.file.pc.load().to_le_bytes();
+    // Push [SP] <- upper(PC + 2)
+    cpu.pushbyte(pc[1]);
 
     // Proceed
-    Ok(Some(Call::Delay(a16).into()))
+    Ok(Some(Call::Push1(a16, pc[0]).into()))
 }
 
-fn delay(_: u8, _: &mut Cpu, a16: u16) -> Return {
-    // Delay by 1 cycle
+fn push1(_: u8, cpu: &mut Cpu, a16: u16, pc0: u8) -> Return {
+    // Push [SP - 1] <- lower(PC + 2)
+    cpu.pushbyte(pc0);
 
     // Proceed
     Ok(Some(Call::Jump(a16).into()))
--- core/src/hw/cpu/sm83/insn/exec/ret.rs
+++ core/src/hw/cpu/sm83/insn/exec/ret.rs
@@ -4,27 +4,28 @@ use remus::Cell;
 use super::{Cpu, Error, Execute, Flag, Operation, Return};
 
 pub const fn default() -> Operation {
-    Operation::Ret(Ret::Fetch)
+    Operation::Ret(Ret::Check)
 }
 
 #[derive(Clone, Debug, Default)]
 pub enum Ret {
     #[default]
-    Fetch,
-    Check(bool),
-    Pop,
-    Delay(u16),
+    Check,
+    Pop0,
+    Pop1(u8),
     Jump(u16),
+    Done,
 }
 
 impl Execute for Ret {
+    #[rustfmt::skip]
     fn exec(self, code: u8, cpu: &mut Cpu) -> Return {
         match self {
-            Self::Fetch => fetch(code, cpu),
-            Self::Check(cond) => check(code, cpu, cond),
-            Self::Pop => pop(code, cpu),
-            Self::Delay(pc) => delay(code, cpu, pc),
-            Self::Jump(pc) => jump(code, cpu, pc),
+            Self::Check     => check(code, cpu),
+            Self::Pop0      => pop0(code, cpu),
+            Self::Pop1(pc0) => pop1(code, cpu, pc0),
+            Self::Jump(pc)  => jump(code, cpu, pc),
+            Self::Done      => done(code, cpu),
         }
     }
 }
@@ -35,48 +36,45 @@ impl From<Ret> for Operation {
     }
 }
 
-fn fetch(code: u8, cpu: &mut Cpu) -> Return {
+fn check(code: u8, cpu: &mut Cpu) -> Return {
     // Evaluate condition
     let flags = &cpu.file.f.load();
+    #[rustfmt::skip]
     let cond = match code {
         0xc0 => !Flag::Z.get(flags),
-        0xc8 => Flag::Z.get(flags),
-        0xc9 => true,
+        0xc8 =>  Flag::Z.get(flags),
         0xd0 => !Flag::C.get(flags),
-        0xd8 => Flag::C.get(flags),
+        0xd8 =>  Flag::C.get(flags),
+        0xc9 => {
+            // Continue
+            return pop0(code, cpu);
+        }
         code => return Err(Error::Opcode(code)),
     };
 
-    if code == 0xc9 {
-        // Continue
-        check(code, cpu, cond)
-    } else {
-        // Proceed
-        Ok(Some(Ret::Check(cond).into()))
-    }
-}
-
-fn check(_: u8, _: &mut Cpu, cond: bool) -> Return {
-    // Execute RET
+    // Check condition
     if cond {
         // Proceed
-        Ok(Some(Ret::Pop.into()))
+        Ok(Some(Ret::Pop0.into()))
     } else {
-        // Finish
-        Ok(None)
+        // Proceed
+        Ok(Some(Ret::Done.into()))
     }
 }
 
-fn pop(_: u8, cpu: &mut Cpu) -> Return {
-    // Pop PC <- [SP]
-    let pc = cpu.popword();
+fn pop0(_: u8, cpu: &mut Cpu) -> Return {
+    // Pop lower(PC) <- [SP]
+    let pc0 = cpu.popbyte();
 
     // Proceed
-    Ok(Some(Ret::Delay(pc).into()))
+    Ok(Some(Ret::Pop1(pc0).into()))
 }
 
-fn delay(_: u8, _: &mut Cpu, pc: u16) -> Return {
-    // Delay by 1 cycle
+fn pop1(_: u8, cpu: &mut Cpu, pc0: u8) -> Return {
+    // Pop upper(PC) <- [SP + 1]
+    let pc1 = cpu.popbyte();
+    // Combine into PC
+    let pc = u16::from_le_bytes([pc0, pc1]);
 
     // Proceed
     Ok(Some(Ret::Jump(pc).into()))
@@ -86,6 +84,11 @@ fn jump(_: u8, cpu: &mut Cpu, pc: u16) -> Return {
     // Perform jump
     cpu.file.pc.store(pc);
 
+    // Proceed
+    Ok(Some(Ret::Done.into()))
+}
+
+fn done(_: u8, _: &mut Cpu) -> Return {
     // Finish
     Ok(None)
 }
