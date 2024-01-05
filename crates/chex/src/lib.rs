bcb111510517e609ee30aeac0236c6fa38f761dd
Zakhary Kaplan <me@zakhary.dev>
2024-01-05 00:00:00 -0500

    feat(chex): support named colors

--- crates/chex/src/lib.rs
+++ crates/chex/src/lib.rs
@@ -22,6 +22,7 @@
 //! ```
 
 #![warn(clippy::pedantic)]
+#![allow(clippy::unreadable_literal)]
 
 use std::fmt::Display;
 use std::num::ParseIntError;
@@ -29,6 +30,8 @@ use std::str::FromStr;
 
 use serde_with::{DeserializeFromStr, SerializeDisplay};
 use thiserror::Error;
+
+mod names;
 
 /// Use [24-bit] color (stored as `0x00RRGGBB_u32`)
 ///
@@ -83,22 +86,25 @@ impl FromStr for Color {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.chars().next().ok_or(Error::Empty)? {
-            '#' => {
-                let input = s.get(1..).ok_or(Error::Unsupported)?;
-                let hex = u32::from_str_radix(input, 16).map_err(Error::ParseInt)?;
-                match input.len() {
-                    3 => Ok({
-                        let r = (hex & 0xf00) * 0x1100;
-                        let g = (hex & 0x0f0) * 0x0110;
-                        let b = (hex & 0x00f) * 0x0011;
-                        Self(r | g | b)
-                    }),
-                    6 => Ok(Self(hex)),
-                    _ => Err(Error::Unsupported),
-                }
+        if s.is_empty() {
+            Err(Error::Empty)
+        } else if s.starts_with('#') {
+            let hex = s.get(1..).ok_or(Error::Unsupported)?;
+            let val = u32::from_str_radix(hex, 16)?;
+            match hex.len() {
+                3 => Ok({
+                    let r = (val & 0xf00) * 0x1100;
+                    let g = (val & 0x0f0) * 0x0110;
+                    let b = (val & 0x00f) * 0x0011;
+                    Self(r | g | b)
+                }),
+                6 => Ok(Self(val)),
+                _ => Err(Error::Unsupported),
             }
-            _ => Err(Error::MissingHash),
+        } else if let Some(color) = names::COLORS.get(s) {
+            Ok(*color)
+        } else {
+            Err(Error::Unknown(s.to_string()))
         }
     }
 }
@@ -107,16 +113,16 @@ impl FromStr for Color {
 #[derive(Clone, Debug, Error)]
 pub enum Error {
     /// Parse string was empty.
-    #[error("could not parse empty string")]
+    #[error("empty string")]
     Empty,
-    /// Parse string does not start with `#`
-    #[error("must start with \"#\"")]
-    MissingHash,
     /// Error parsing hexadecimal from input.
     #[error("could not parse hex")]
     ParseInt(#[from] ParseIntError),
-    /// Use of an unsupported format.
-    #[error("unsupported color format")]
+    /// Use of an unknown format.
+    #[error("unknown color name: {0}")]
+    Unknown(String),
+    /// Use of an unsupported hex format.
+    #[error("unsupported hex format")]
     Unsupported,
 }
 
@@ -135,34 +141,18 @@ mod tests {
         assert_eq!(   "#123".parse::<Color>().unwrap(), Color(0x112233));
         assert_eq!("#123456".parse::<Color>().unwrap(), Color(0x123456));
         assert_eq!("#AbCdEf".parse::<Color>().unwrap(), Color(0xabcdef));
+        assert_eq!("crimson".parse::<Color>().unwrap(), Color(0xdc143c));
     }
 
+    #[rustfmt::skip]
     #[test]
     fn parse_errors() {
-        assert!(matches!("".parse::<Color>().unwrap_err(), Error::Empty));
-        assert!(matches!(
-            "shalom".parse::<Color>().unwrap_err(),
-            Error::MissingHash
-        ));
-        assert!(matches!(
-            "#".parse::<Color>().unwrap_err(),
-            Error::ParseInt(_)
-        ));
-        assert!(matches!(
-            "#shalom".parse::<Color>().unwrap_err(),
-            Error::ParseInt(_)
-        ));
-        assert!(matches!(
-            "#12".parse::<Color>().unwrap_err(),
-            Error::Unsupported
-        ));
-        assert!(matches!(
-            "#1234".parse::<Color>().unwrap_err(),
-            Error::Unsupported
-        ));
-        assert!(matches!(
-            "#1234567".parse::<Color>().unwrap_err(),
-            Error::Unsupported
-        ));
+        assert!(matches!(        "".parse::<Color>(), Err(Error::Empty)));
+        assert!(matches!(  "shalom".parse::<Color>(), Err(Error::Unknown(_))));
+        assert!(matches!(  "#error".parse::<Color>(), Err(Error::ParseInt(_))));
+        assert!(matches!(       "#".parse::<Color>(), Err(Error::ParseInt(_))));
+        assert!(matches!(     "#12".parse::<Color>(), Err(Error::Unsupported)));
+        assert!(matches!(   "#1234".parse::<Color>(), Err(Error::Unsupported)));
+        assert!(matches!("#1234567".parse::<Color>(), Err(Error::Unsupported)));
     }
 }
