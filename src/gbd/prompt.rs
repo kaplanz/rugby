29158f6a166fb1c1caabbca014a09fdd85036eac
Zakhary Kaplan <me@zakhary.dev>
2024-02-02 00:00:00 -0500

    docs(gbd): update, document missing items

--- src/gbd/prompt.rs
+++ src/gbd/prompt.rs
@@ -1,24 +1,31 @@
+//! Generalized debugger prompt.
+
 use std::fmt::Debug;
 
 use thiserror::Error;
 
 /// Behaviour for prompting a user for input.
 pub trait Prompt: Debug + Send {
-    /// Prompt the user for input.
+    /// Present the prompt message and receive a debugger command from the user.
     ///
     /// # Errors
     ///
     /// Returns an error if the prompt fails to produce input. If the user
     /// wishes to terminate the program, the special error [`Error::Quit`] will
     /// be returned.
-    fn prompt(&mut self, ask: &str) -> Result<String, Error>;
+    fn prompt(&mut self, msg: &str) -> Result<String, Error>;
 }
 
 /// A type specifying categories of [`Prompt`] errors.
 #[derive(Debug, Error)]
 pub enum Error {
+    /// Generic internal error.
     #[error(transparent)]
     Internal(#[from] Box<dyn std::error::Error>),
+    /// Quit request.
+    ///
+    /// Special error used to signal to the debugger that the uesr has requested
+    /// to quit the program.
     #[error("quit requested by user")]
     Quit,
 }
