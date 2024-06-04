ad1c196a2010e609ff3ef12e51f171e7d231f1e6
Zakhary Kaplan <me@zakhary.dev>
2024-06-04 00:00:00 -0400

    feat(web): initial lib

--- apps/web/src/lib.rs
+++ apps/web/src/lib.rs
@@ -1,11 +1,34 @@
+use rugby::arch::Block;
+use rugby::core::dmg;
 use wasm_bindgen::prelude::*;
 
+#[derive(Debug, Default)]
+#[wasm_bindgen(inspectable)]
+pub struct GameBoy(dmg::GameBoy);
+
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(msg: &str);
+impl GameBoy {
+    /// Constructs a new `GameBoy`.
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        Self(dmg::GameBoy::new())
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+impl GameBoy {
+    /// Checks if enabled.
+    pub fn ready(&mut self) -> bool {
+        self.0.ready()
+    }
+
+    /// Emulates a single cycle.
+    pub fn cycle(&mut self) {
+        self.0.cycle();
+    }
+
+    /// Performs a reset.
+    pub fn reset(&mut self) {
+        self.0.reset();
+    }
 }
