c60afa7d27a892dd192da9287f562222e82ba274
Zakhary Kaplan <me@zakhary.dev>
2024-06-20 00:00:00 -0400

    feat(ios): init xcode

---
+++ apps/ios/Rugby/RugbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RugbyApp.swift
+//  Rugby
+//
+//  Created by Zakhary Kaplan on 2024-06-20.
+//
+
+import SwiftUI
+
+@main
+struct RugbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
