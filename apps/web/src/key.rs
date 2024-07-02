15dd699632535a213742129c61490fe530452eaf
Zakhary Kaplan <me@zakhary.dev>
2024-07-01 22:00:00 -0400

    feat(web): process keymap in frontend

--- apps/web/src/key.rs
+++ apps/web/src/key.rs
@@ -1,38 +1,48 @@
-use log::debug;
-use rugby::core::dmg::Button;
+use rugby::core::dmg;
 use rugby::emu::part::joypad::State;
 use rugby::prelude::*;
 use wasm_bindgen::prelude::*;
 
 use crate::emu::GameBoy;
 
-#[rustfmt::skip]
-fn keymap(key: &str) -> Option<Button> {
-    match key {
-        "x"          => Some(Button::A),
-        "z"          => Some(Button::B),
-        " "          => Some(Button::Select),
-        "Enter"      => Some(Button::Start),
-        "ArrowRight" => Some(Button::Right),
-        "ArrowLeft"  => Some(Button::Left),
-        "ArrowUp"    => Some(Button::Up),
-        "ArrowDown"  => Some(Button::Down),
-        _ => {
-            debug!("unknown key: {key}");
-            None
-        }
+#[wasm_bindgen]
+impl GameBoy {
+    pub fn press(&mut self, key: Button) {
+        let event = Some((dmg::Button::from(key), State::Dn).into());
+        self.0.inside_mut().joypad().recv(event);
+    }
+
+    pub fn release(&mut self, key: Button) {
+        let event = Some((dmg::Button::from(key), State::Up).into());
+        self.0.inside_mut().joypad().recv(event);
     }
 }
 
 #[wasm_bindgen]
-impl GameBoy {
-    pub fn keydown(&mut self, key: &str) {
-        let event = keymap(key).map(|key| (key, State::Dn).into());
-        self.0.inside_mut().joypad().recv(event);
-    }
+#[derive(Copy, Clone, Debug)]
+pub enum Button {
+    A,
+    B,
+    Select,
+    Start,
+    Right,
+    Left,
+    Up,
+    Down,
+}
 
-    pub fn keyup(&mut self, key: &str) {
-        let event = keymap(key).map(|key| (key, State::Up).into());
-        self.0.inside_mut().joypad().recv(event);
+#[rustfmt::skip]
+impl From<Button> for dmg::Button {
+    fn from(value: Button) -> Self {
+        match value {
+            Button::A      => dmg::Button::A,
+            Button::B      => dmg::Button::B,
+            Button::Select => dmg::Button::Select,
+            Button::Start  => dmg::Button::Start,
+            Button::Right  => dmg::Button::Right,
+            Button::Left   => dmg::Button::Left,
+            Button::Up     => dmg::Button::Up,
+            Button::Down   => dmg::Button::Down,
+        }
     }
 }
