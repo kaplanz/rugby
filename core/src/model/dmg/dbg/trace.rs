97182284740ff07eecfdd73bb09e4cea2c9d97a0
Zakhary Kaplan <me@zakhary.dev>
2024-07-12 00:00:00 -0400

    refactor: improve readability of tracing impl

--- core/src/model/dmg/dbg/trace.rs
+++ core/src/model/dmg/dbg/trace.rs
@@ -1,64 +1,36 @@
 //! Introspective tracing.
 
-use std::fmt::Write;
+use rugby_arch::reg::Register;
 
-use rugby_arch::reg::{Port, Register};
-use rugby_arch::{Byte, Word};
-
-use super::cpu::{Flag, Select16, Select8};
+use super::cpu::Flag;
 use super::GameBoy;
-use crate::api::core::Core;
 
 /// Collect a trace with formatting matching [binjgb].
 ///
 /// [binjgb]: https://github.com/binji/binjgb
 #[must_use]
 pub fn binjgb(emu: &GameBoy) -> String {
-    let mut repr = String::new();
-    let cpu = emu.inside().proc();
-    write!(&mut repr, "A:{:02X} ", Port::<Byte>::load(cpu, Select8::A)).unwrap();
-    write!(&mut repr, "F:{} ", {
-        let flags = Port::<Byte>::load(cpu, Select8::F);
-        format!(
-            "{}{}{}{}",
-            if Flag::Z.get(&flags) { "Z" } else { "-" },
-            if Flag::N.get(&flags) { "N" } else { "-" },
-            if Flag::H.get(&flags) { "H" } else { "-" },
-            if Flag::C.get(&flags) { "C" } else { "-" },
-        )
-    })
-    .unwrap();
-    write!(
-        &mut repr,
-        "BC:{:04X} ",
-        Port::<Word>::load(cpu, Select16::BC)
-    )
-    .unwrap();
-    write!(
-        &mut repr,
-        "DE:{:04x} ",
-        Port::<Word>::load(cpu, Select16::DE)
-    )
-    .unwrap();
-    write!(
-        &mut repr,
-        "HL:{:04x} ",
-        Port::<Word>::load(cpu, Select16::HL)
-    )
-    .unwrap();
-    write!(
-        &mut repr,
-        "SP:{:04x} ",
-        Port::<Word>::load(cpu, Select16::SP)
-    )
-    .unwrap();
-    write!(
-        &mut repr,
-        "PC:{:04x}",
-        Port::<Word>::load(cpu, Select16::PC)
-    )
-    .unwrap();
-    repr
+    let cpu = &emu.main.soc.cpu;
+    [
+        format!("A:{:02X}", cpu.reg.a),
+        format!("F:{}", {
+            [Flag::Z, Flag::N, Flag::H, Flag::C]
+                .map(|flag| {
+                    if flag.get(&cpu.reg.f) {
+                        format!("{flag:?}")
+                    } else {
+                        "-".to_string()
+                    }
+                })
+                .join("")
+        }),
+        format!("BC:{:04X}", cpu.reg.bc().load()),
+        format!("DE:{:04x}", cpu.reg.de().load()),
+        format!("HL:{:04x}", cpu.reg.hl().load()),
+        format!("SP:{:04x}", cpu.reg.sp),
+        format!("PC:{:04x}", cpu.reg.pc),
+    ]
+    .join(" ")
 }
 
 /// Collect a trace with formatting as specified by [Gameboy Doctor][gbdoc].
@@ -66,26 +38,26 @@ pub fn binjgb(emu: &GameBoy) -> String {
 /// [gbdoc]: https://robertheaton.com/gameboy-doctor
 #[must_use]
 pub fn doctor(emu: &GameBoy) -> String {
-    let mut repr = String::new();
-    let cpu = emu.inside().proc();
-    write!(&mut repr, "A:{:02X} ", cpu.reg.a.load()).unwrap();
-    write!(&mut repr, "F:{:02X} ", cpu.reg.f.load()).unwrap();
-    write!(&mut repr, "B:{:02X} ", cpu.reg.b.load()).unwrap();
-    write!(&mut repr, "C:{:02X} ", cpu.reg.c.load()).unwrap();
-    write!(&mut repr, "D:{:02X} ", cpu.reg.d.load()).unwrap();
-    write!(&mut repr, "E:{:02X} ", cpu.reg.e.load()).unwrap();
-    write!(&mut repr, "H:{:02X} ", cpu.reg.h.load()).unwrap();
-    write!(&mut repr, "L:{:02X} ", cpu.reg.l.load()).unwrap();
-    write!(&mut repr, "SP:{:04X} ", cpu.reg.sp.load()).unwrap();
-    write!(&mut repr, "PC:{:04X} ", cpu.reg.pc.load()).unwrap();
-    let pcmem = [0, 1, 2, 3]
-        .map(|i| cpu.reg.pc.load() + i)
-        .map(|addr| cpu.read(addr));
-    write!(
-        &mut repr,
-        "PCMEM:{:02X},{:02X},{:02X},{:02X}",
-        pcmem[0], pcmem[1], pcmem[2], pcmem[3],
-    )
-    .unwrap();
-    repr
+    let cpu = &emu.main.soc.cpu;
+    [
+        format!("A:{:02X}", cpu.reg.a.load()),
+        format!("F:{:02X}", cpu.reg.f.load()),
+        format!("B:{:02X}", cpu.reg.b.load()),
+        format!("C:{:02X}", cpu.reg.c.load()),
+        format!("D:{:02X}", cpu.reg.d.load()),
+        format!("E:{:02X}", cpu.reg.e.load()),
+        format!("H:{:02X}", cpu.reg.h.load()),
+        format!("L:{:02X}", cpu.reg.l.load()),
+        format!("SP:{:04X}", cpu.reg.sp.load()),
+        format!("PC:{:04X}", cpu.reg.pc.load()),
+        format!(
+            "PCMEM:{}",
+            [0, 1, 2, 3]
+                .map(|offs| cpu.reg.pc.load() + offs)
+                .map(|addr| cpu.read(addr))
+                .map(|data| format!("{data:02X}"))
+                .join(","),
+        ),
+    ]
+    .join(" ")
 }
