e0b267b8b681b2564c9e938efc7edebba8095b2c
Zakhary Kaplan <me@zakhary.dev>
2024-07-15 20:30:00 -0400

    refactor: remove `Ppu::screen`

--- tests/mealybug.rs
+++ tests/mealybug.rs
@@ -1,5 +1,6 @@
 use rugby::arch::Block;
 use rugby::core::dmg::{Cartridge, GameBoy};
+use rugby::prelude::*;
 
 mod common;
 
@@ -22,7 +23,7 @@ fn emulate(rom: &[u8], img: &[u8]) -> Result<()> {
         emu.cycle();
     }
     // Calculate difference
-    let delta = image::cmp(emu.main.soc.ppu.screen(), img);
+    let delta = image::cmp(emu.main.soc.ppu.frame(), img);
     let total = img.len();
 
     // Check for success
