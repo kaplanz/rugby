25dbe5e071a3aa3ac6358c1d07ff61c170dde424
Zakhary Kaplan <me@zakhary.dev>
2024-11-28 00:00:00 -0500

    feat(web): work on style

--- apps/web/www/lib/Stereo.svelte
+++ apps/web/www/lib/Stereo.svelte
@@ -12,11 +12,12 @@
     display: inline-flex;
     flex-direction: row;
     gap: 10px;
+    padding: 4px;
     transform: rotate(-30deg);
   }
 
   span {
-    background-color: black;
+    background-color: light-dark(#1c1a19, #5f5e61);
     border-radius: 3px;
     height: 60px;
     width: 5px;
