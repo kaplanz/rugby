fca66a8dd50d5e842ed035525863f79b281d5e01
Zakhary Kaplan <me@zakhary.dev>
2024-12-16 22:00:00 -0500

    feat(ios): init uniffi

--- apps/ios/src/lib.rs
+++ apps/ios/src/lib.rs
@@ -1,4 +1,12 @@
+use rugby::core::dmg;
+
 uniffi::setup_scaffolding!();
+
+#[uniffi::export]
+#[derive(uniffi::Record)]
+pub struct GameBoy {
+    pub(crate) inner: dmg::GameBoy,
+}
 
 #[uniffi::export]
 pub fn hello(name: &str) -> String {
