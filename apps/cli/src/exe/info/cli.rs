6e020aab1e0f41282c1211e4b457c0bf8fb3c41d
Zakhary Kaplan <me@zakhary.dev>
2024-12-19 18:30:00 -0500

    feat(cli): support generating manual pages for commands

--- apps/cli/src/exe/info/cli.rs
+++ apps/cli/src/exe/info/cli.rs
@@ -1,10 +1,10 @@
 //! Command-line interface.
 
-use clap::Args;
+use clap::Parser;
 use rugby_cfg::opt::emu::Cart;
 
 /// [Info](super::exec) options.
-#[derive(Args, Debug)]
+#[derive(Debug, Parser)]
 #[clap(arg_required_else_help = true)]
 #[group(id = "Info")]
 pub struct Cli {
