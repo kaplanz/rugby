871f597acf8be669af2eea735e5506cfe2b9806d
Zakhary Kaplan <me@zakhary.dev>
2024-12-30 23:00:00 -0500

    feat(web): link to build commit

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,9 +1,16 @@
+import { execSync } from "node:child_process";
+
 import { defineConfig } from "vite";
 
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    BUILD: {
+      COMMIT: execSync("git rev-parse --short HEAD").toString().trim(),
+    },
+  },
   esbuild: {
     supported: {
       "top-level-await": true,
