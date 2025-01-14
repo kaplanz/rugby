5f5f525ee8f50ce9f8f024086cd2f8be54c44cf3
Zakhary Kaplan <me@zakhary.dev>
2025-01-14 00:00:00 -0500

    feat(ios): initial app

---
+++ apps/ios/Rugby/Views/Library/GameInfo.swift
@@ -0,0 +1,224 @@
+//
+//  GameInfo.swift
+//  Rugby
+//
+//  Created by Zakhary Kaplan on 2025-01-10.
+//
+
+import Algorithms
+import CryptoKit
+import RugbyKit
+import SwiftUI
+import zlib
+
+struct GameInfo: View {
+    @Environment(GameBoy.self) var emu
+    @Environment(\.dismiss) var dismiss
+
+    /// Game instance.
+    @State var game: Game
+
+    /// Game cartridge.
+    var cart: Cartridge {
+        .init(rom: game.data)
+    }
+
+    /// Game information.
+    var info: Info {
+        cart.info()
+    }
+
+    var body: some View {
+        List {
+            Section {
+                HStack {
+                    Spacer()
+                    GameIcon(game: game)
+                        .shadow(radius: 4)
+                    Spacer()
+                }
+                Text(game.name)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                Button("Play") {
+                    dismiss()
+                    emu.play(game)
+                }
+                .bold()
+                .buttonStyle(.borderedProminent)
+                .clipShape(.rect(cornerRadius: .infinity))
+            }
+            .listRowSeparator(.hidden, edges: .top)
+            Section("Information") {
+                Row("Title") {
+                    Text(info.title ?? "Unknown")
+                }
+                Row("Version") {
+                    Text(info.version)
+                }
+                Row("Region") {
+                    Text(info.region)
+                }
+                Row("Compatible") {
+                    let support = [
+                        (title: "DMG", allow: info.dmg, color: Color.blue),
+                        (title: "CGB", allow: info.cgb, color: Color.purple),
+                        (title: "SGB", allow: info.sgb, color: Color.red),
+                    ]
+                    .filter { $0.allow }
+                    ForEach(support, id: \.title) {
+                        Badge(title: $0.title, color: $0.color)
+                    }
+                }
+            }
+            Section("Cartridge") {
+                Row("Kind") {
+                    Text(info.cart)
+                }
+                Row("ROM") {
+                    Text(info.romsz)
+                }
+                Row("RAM") {
+                    Text(info.ramsz)
+                }
+            }
+            Section("Checksum") {
+                Row("Header") {
+                    Text(String(format: "%02X", info.hchk))
+                        .monospaced()
+                        .textSelection(.enabled)
+                }
+                Row("Global") {
+                    Text(String(format: "%04X", info.gchk))
+                        .monospaced()
+                        .textSelection(.enabled)
+                }
+                Row("CRC32") {
+                    let hash = game.data.withUnsafeBytes {
+                        crc32(0, $0.bindMemory(to: UInt8.self).baseAddress, uInt(game.data.count))
+                    }
+                    let repr = String(format: "%08X", hash)
+                    Text(repr)
+                        .monospaced()
+                        .textSelection(.enabled)
+                }
+                Row("MD5") {
+                    let hash = Insecure.MD5.hash(data: game.data)
+                    let repr = hash.map { String(format: "%02X", $0) }.chunks(ofCount: 4).map {
+                        $0.joined()
+                    }.joined(separator: " ")
+                    Text(repr)
+                        .monospaced()
+                        .textSelection(.enabled)
+                }
+                Row("SHA-1") {
+                    let hash = Insecure.SHA1.hash(data: game.data)
+                    let repr = hash.map { String(format: "%02X", $0) }.chunks(ofCount: 4).map {
+                        $0.joined()
+                    }.joined(separator: " ")
+                    Text(repr)
+                        .monospaced()
+                        .textSelection(.enabled)
+                }
+                Row("SHA-256") {
+                    let hash = SHA256.hash(data: game.data)
+                    let repr = hash.map { String(format: "%02X", $0) }.chunks(ofCount: 4).map {
+                        $0.joined()
+                    }.joined(separator: " ")
+                    Text(repr)
+                        .monospaced()
+                        .textSelection(.enabled)
+                }
+            }
+        }
+        .listStyle(.plain)
+        .environment(\.defaultMinListRowHeight, 32.5)
+    }
+}
+
+#Preview {
+    GameInfo(
+        game: Game(
+            path: Bundle.main.url(
+                forResource: "roms/games/porklike/porklike",
+                withExtension: "gb"
+            )!
+        )
+    )
+    .environment(GameBoy())
+}
+
+private struct Section<Content: View>: View {
+    let title: String?
+    let content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.title = nil
+        self.content = content()
+    }
+
+    init(_ title: String, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
+    }
+
+    var body: some View {
+        SwiftUI.Section {
+            content
+        } header: {
+            if let title = title {
+                Text(title)
+                    .font(.title3)
+                    .bold()
+                    .foregroundStyle(Color.primary)
+            }
+        }
+    }
+}
+
+private struct Row<Content: View>: View {
+    let title: String
+    let value: Content
+
+    init(_ title: String, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.value = content()
+    }
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .foregroundStyle(.secondary)
+            Spacer()
+            value
+                .multilineTextAlignment(.trailing)
+        }
+        .font(.footnote)
+        .textSelection(.enabled)
+        .listRowSeparator(.hidden, edges: .top)
+        .listRowSeparator(.visible, edges: .bottom)
+        .alignmentGuide(.listRowSeparatorLeading) { row in
+            row[.leading]
+        }
+        .alignmentGuide(.listRowSeparatorTrailing) { row in
+            row[.trailing]
+        }
+    }
+}
+
+private struct Badge: View {
+    let title: String
+    let color: Color
+
+    var body: some View {
+        Text(title)
+            .font(.caption2)
+            .fontWeight(.medium)
+            .foregroundStyle(color)
+            .padding(3)
+            .overlay {
+                RoundedRectangle(cornerRadius: 4)
+                    .stroke(color, lineWidth: 1.5)
+            }
+    }
+}
--- apps/ios/src/lib.rs
+++ apps/ios/src/lib.rs
@@ -1,6 +1,93 @@
+use std::sync::RwLock;
+
+use rugby::arch::Block;
+use rugby::core::dmg;
+use rugby::prelude::*;
+use uniffi::Object;
+
 uniffi::setup_scaffolding!();
 
+pub mod cart;
+
+/// Game Boy model.
+#[derive(Debug, Default, Object)]
+pub struct GameBoy(RwLock<inner::Model>);
+
 #[uniffi::export]
-pub fn hello(name: &str) -> String {
-    format!("Hello, {name}!")
+impl GameBoy {
+    /// Constructs a new `GameBoy`.
+    #[uniffi::constructor]
+    pub fn new() -> Self {
+        Self(RwLock::new(inner::Model(dmg::GameBoy::new())))
+    }
+
+    /// Checks if enabled.
+    #[uniffi::method]
+    pub fn ready(&self) -> bool {
+        self.0.read().unwrap().ready()
+    }
+
+    /// Emulates a single cycle.
+    #[uniffi::method]
+    pub fn cycle(&self) {
+        self.0.write().unwrap().cycle();
+    }
+
+    /// Performs a reset.
+    #[uniffi::method]
+    pub fn reset(&self) {
+        self.0.write().unwrap().reset();
+    }
+}
+
+#[uniffi::export]
+impl GameBoy {
+    /// Checks if the frame is ready to be rendered.
+    pub fn vsync(&self) -> bool {
+        self.0.read().unwrap().inside().video().vsync()
+    }
+
+    /// Retrieves the current frame state.
+    pub fn frame(&self) -> Vec<u8> {
+        self.0
+            .read()
+            .unwrap()
+            .inside()
+            .video()
+            .frame()
+            .iter()
+            .map(|&pix| pix as u8)
+            .collect::<Vec<u8>>()
+    }
+}
+
+/// Private model implementation.
+mod inner {
+    use std::ops::{Deref, DerefMut};
+
+    use rugby::core::dmg::GameBoy;
+
+    /// Inner emulator model.
+    #[derive(Debug, Default)]
+    pub struct Model(pub(crate) GameBoy);
+
+    impl Deref for Model {
+        type Target = GameBoy;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    impl DerefMut for Model {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
+
+    // SAFETY: `GameBoy` does not expose its thread-unsafe internals.
+    unsafe impl Send for Model {}
+
+    // SAFETY: `GameBoy` does not expose its thread-unsafe internals.
+    unsafe impl Sync for Model {}
 }
