09efc4d5aab5a32eae19efb3591683fe9f2b8b80
Zakhary Kaplan <me@zakhary.dev>
2025-01-19 12:30:00 -0500

    fix(ios): properly eject game cartridge

--- apps/ios/src/api/video.rs
+++ apps/ios/src/api/video.rs
@@ -11,7 +11,7 @@ impl GameBoy {
     /// Assuming the PPU is enabled (by the cartridge), this should be true
     /// exactly once every 70,224 cycles.
     pub fn vsync(&self) -> bool {
-        self.0.read().unwrap().inside().video().vsync()
+        self.emu.read().unwrap().inside().video().vsync()
     }
 
     /// Gets the current frame state.
@@ -30,7 +30,7 @@ impl GameBoy {
     /// `(20 * 160) + 16  = 3216`th element of the frame (zero indexed) as
     /// `frame[3215]`.
     pub fn frame(&self) -> Vec<u8> {
-        self.0
+        self.emu
             .read()
             .unwrap()
             .inside()
