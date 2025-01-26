51035219cd1dcb191dbb2bd67d8fd039535ce1f0
Zakhary Kaplan <me@zakhary.dev>
2025-01-26 18:30:00 -0500

    revert(ios): restore default tint
    This reverts commit 2efb38c095f61b01f1735682fe4cb9796af5910d.

--- apps/ios/Rugby/Views/MainView.swift
+++ apps/ios/Rugby/Views/MainView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct MainView: View {
     @Environment(GameBoy.self) private var emu
-    @Environment(\.colorScheme) var colorScheme
 
     /// Manage application settings.
     @State private var manage = false
@@ -41,30 +40,6 @@ struct MainView: View {
                 EmulatorView()
             }
         }
-        .tint(tint)
-    }
-
-    var tint: Color {
-        // Define palette color
-        let hex = emu.cfg.data.pal.data.last!
-        let rgb = (
-            r: Double((hex >> 16) & 0xFF) / 255,
-            g: Double((hex >> 08) & 0xFF) / 255,
-            b: Double((hex >> 00) & 0xFF) / 255
-        )
-        // Define tinting color
-        let hsl = (
-            h: (1 +
-                atan2(
-                    sqrt(3) * (rgb.g - rgb.b),
-                    2 * rgb.r - rgb.g - rgb.b
-                ) / (2 * .pi)
-            ).truncatingRemainder(dividingBy: 1.0),
-            s: (rgb.r == rgb.g) && (rgb.g == rgb.b) ? 0 : 0.6,
-            l: colorScheme == .light ? 0.6 : 0.9
-        )
-
-        return Color(hue: hsl.h, saturation: hsl.s, brightness: hsl.l)
     }
 }
 
