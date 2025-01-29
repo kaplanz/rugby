dcdc70a2a129de60731caf2b9f3ac74c7d056ef7
Zakhary Kaplan <me@zakhary.dev>
2025-01-28 21:00:00 -0500

    feat(apu): prepare initial impl

---
+++ core/src/parts/apu/ch2.rs
@@ -0,0 +1,1 @@
+//! Channel 2: Square.
--- core/src/parts/apu/mod.rs
+++ core/src/parts/apu/mod.rs
@@ -3,9 +3,14 @@
 use rugby_arch::mem::Ram;
 use rugby_arch::mio::{Bus, Mmio};
 use rugby_arch::reg::{Port, Register};
-use rugby_arch::{Block, Byte, Shared};
+use rugby_arch::{Bitmask, Block, Byte, Shared};
 
 use crate::api::part::audio::Audio as Api;
+
+pub mod ch1;
+pub mod ch2;
+pub mod ch3;
+pub mod ch4;
 
 /// Wave RAM.
 ///
@@ -21,47 +26,47 @@ pub type Wave = Ram<[Byte; 0x0010]>;
 /// [regs]: https://gbdev.io/pandocs/Audio_Registers.html
 #[derive(Clone, Copy, Debug)]
 pub enum Select {
-    /// `[$FF26]`: Audio master control
+    /// `[$FF26]`: Audio master control.
     Nr52,
-    /// `[$FF25]`: Sound panning
+    /// `[$FF25]`: Sound panning.
     Nr51,
-    /// `[$FF24]`: Master volume & VIN panning
+    /// `[$FF24]`: Master volume & VIN panning.
     Nr50,
-    /// `[$FF10]`: CH1 period sweep
+    /// `[$FF10]`: CH1 period sweep.
     Nr10,
-    /// `[$FF11]`: CH1 length timer & duty cycle
+    /// `[$FF11]`: CH1 length timer & duty cycle.
     Nr11,
-    /// `[$FF12]`: CH1 volume & envelope
+    /// `[$FF12]`: CH1 volume & envelope.
     Nr12,
-    /// `[$FF13]`: CH1 period low
+    /// `[$FF13]`: CH1 period low.
     Nr13,
-    /// `[$FF14]`: CH1 period high & control
+    /// `[$FF14]`: CH1 period high & control.
     Nr14,
-    /// `[$FF16]`: CH2 length timer & duty cycle
+    /// `[$FF16]`: CH2 length timer & duty cycle.
     Nr21,
-    /// `[$FF17]`: CH2 volume & envelope
+    /// `[$FF17]`: CH2 volume & envelope.
     Nr22,
-    /// `[$FF18]`: CH2 period low
+    /// `[$FF18]`: CH2 period low.
     Nr23,
-    /// `[$FF19]`: CH2 period high & control
+    /// `[$FF19]`: CH2 period high & control.
     Nr24,
-    /// `[$FF1A]`: CH3 DAC enable
+    /// `[$FF1A]`: CH3 DAC enable.
     Nr30,
-    /// `[$FF1B]`: CH3 length timer
+    /// `[$FF1B]`: CH3 length timer.
     Nr31,
-    /// `[$FF1C]`: CH3 output level
+    /// `[$FF1C]`: CH3 output level.
     Nr32,
-    /// `[$FF1D]`: CH3 period low
+    /// `[$FF1D]`: CH3 period low.
     Nr33,
-    /// `[$FF1E]`: CH3 period high & control
+    /// `[$FF1E]`: CH3 period high & control.
     Nr34,
-    /// `[$FF20]`: CH4 length timer
+    /// `[$FF20]`: CH4 length timer.
     Nr41,
-    /// `[$FF21]`: CH4 volume & envelope
+    /// `[$FF21]`: CH4 volume & envelope.
     Nr42,
-    /// `[$FF22]`: CH4 frequency & randomness
+    /// `[$FF22]`: CH4 frequency & randomness.
     Nr43,
-    /// `[$FF23]`: CH4 control
+    /// `[$FF23]`: CH4 control.
     Nr44,
 }
 
@@ -72,7 +77,14 @@ pub struct Apu {
     pub reg: Control,
     /// Audio memory.
     pub mem: Bank,
+    /// Audio internals.
+    #[allow(unused)]
+    etc: Internal,
 }
+
+/// Audio internals.
+#[derive(Debug, Default)]
+struct Internal {}
 
 impl Apu {
     /// Constructs a new `Apu`.
@@ -312,5 +324,177 @@ impl Block for Bank {}
 impl Mmio for Bank {
     fn attach(&self, bus: &mut Bus) {
         bus.map(0xff30..=0xff40, self.wave.clone().into());
+    }
+}
+
+/// Audio master control.
+///
+/// See more details [here][nr52].
+///
+/// [nr52]: https://gbdev.io/pandocs/Audio_Registers.html#ff26--nr52-audio-master-control
+#[derive(Clone, Copy, Debug)]
+pub enum Nr52 {
+    /// `NR52[7]`: Audio enable.
+    ///
+    /// This controls whether the APU is powered on at all (akin to [LCDC bit
+    /// 7][lcdc.7]). Turning the APU off drains less power (around 16%), but
+    /// clears all APU registers and makes them read-only until turned back on,
+    /// except NR521. Turning the APU off, however, does not affect [Wave
+    /// RAM](Wave), which can always be read/written, nor the [DIV-APU] counter.
+    ///
+    /// [lcdc.7]: super::ppu::Lcdc::Enable
+    Enable = 0b1000_0000,
+    /// `NR52[3]`: Channel 4 enabled. (Read-only)
+    ///
+    /// Allows checking whether this channel is active. Writing to this bit does
+    /// **not** enable or disable the channel.
+    Ch4Ena = 0b0000_1000,
+    /// `NR52[2]`: Channel 3 enabled. (Read-only)
+    ///
+    /// Allows checking whether this channel is active. Writing to this bit does
+    /// **not** enable or disable the channel.
+    Ch3Ena = 0b0000_0100,
+    /// `NR52[1]`: Channel 2 enabled. (Read-only)
+    ///
+    /// Allows checking whether this channel is active. Writing to this bit does
+    /// **not** enable or disable the channel.
+    Ch2Ena = 0b0000_0010,
+    /// `NR52[0]`: Channel 1 enabled. (Read-only)
+    ///
+    /// Allows checking whether this channel is active. Writing to this bit does
+    /// **not** enable or disable the channel.
+    Ch1Ena = 0b0000_0001,
+}
+
+impl Bitmask<Nr52> for Byte {
+    fn test(&self, mask: Nr52) -> bool {
+        let mask = mask as Self;
+        self & mask != 0
+    }
+
+    fn set(&mut self, mask: Nr52, value: bool) {
+        let mask = mask as Self;
+        let wide = !Byte::from(value).wrapping_sub(1);
+        *self ^= (*self ^ wide) & mask;
+    }
+}
+
+impl From<Nr52> for Byte {
+    fn from(value: Nr52) -> Self {
+        value as _
+    }
+}
+
+/// Sound panning.
+///
+/// Each channel can be panned hard left, center, hard right, or ignored
+/// entirely.
+///
+/// Setting a bit to 1 enables the channel to go into the selected output.
+///
+/// # Note
+///
+/// Selecting or de-selecting a channel whose [DAC] is enabled will [cause an audio
+/// pop][pop].
+///
+/// See more details [here][nr51].
+///
+/// [dac]:  https://gbdev.io/pandocs/Audio_details.html#dacs
+/// [pop]:  https://gbdev.io/pandocs/Audio_details.html#mixer
+/// [nr51]: https://gbdev.io/pandocs/Audio_Registers.html#ff25--nr51-sound-panning
+#[derive(Clone, Copy, Debug)]
+pub enum Nr51 {
+    /// `NR51[7]`: Channel 4 left.
+    Ch4L = 0b1000_0000,
+    /// `NR51[6]`: Channel 3 left.
+    Ch3L = 0b0100_0000,
+    /// `NR51[5]`: Channel 2 left.
+    Ch2L = 0b0010_0000,
+    /// `NR51[4]`: Channel 1 left.
+    Ch1L = 0b0001_0000,
+    /// `NR51[3]`: Channel 4 right.
+    Ch4R = 0b0000_1000,
+    /// `NR51[2]`: Channel 3 right.
+    Ch3R = 0b0000_0100,
+    /// `NR51[1]`: Channel 2 right.
+    Ch2R = 0b0000_0010,
+    /// `NR51[0]`: Channel 1 right.
+    Ch1R = 0b0000_0001,
+}
+
+impl Bitmask<Nr51> for Byte {
+    fn test(&self, mask: Nr51) -> bool {
+        let mask = mask as Self;
+        self & mask != 0
+    }
+
+    fn set(&mut self, mask: Nr51, value: bool) {
+        let mask = mask as Self;
+        let wide = !Byte::from(value).wrapping_sub(1);
+        *self ^= (*self ^ wide) & mask;
+    }
+}
+
+impl From<Nr51> for Byte {
+    fn from(value: Nr51) -> Self {
+        value as _
+    }
+}
+
+/// Master volume & VIN panning.
+///
+/// See more details [here][nr50].
+///
+/// [nr50]: https://gbdev.io/pandocs/Audio_Registers.html#ff24--nr50-master-volume--vin-panning
+#[derive(Clone, Copy, Debug)]
+pub enum Nr50 {
+    /// `NR50[7]`: VIN left.
+    ///
+    /// Work exactly like the bits in [NR51](Nr51). Should be set at 0 if
+    /// external sound hardware is not being used.
+    VinL = 0b1000_0000,
+    /// `NR50[6]`: Left volume.
+    ///
+    /// Specifies the master volume for the left output.
+    ///
+    /// # Note
+    ///
+    /// A value of 0 is treated as a volume of 1 (very quiet), and a value of 7
+    /// is treated as a volume of 8 (no volume reduction). Importantly, the
+    /// amplifier **never mutes** a non-silent input.
+    VolL = 0b0111_0000,
+    /// `NR50[5]`: VIN right.
+    ///
+    /// Work exactly like the bits in [NR51](Nr51). Should be set at 0 if
+    /// external sound hardware is not being used.
+    VinR = 0b0000_1000,
+    /// `NR50[0]`: Right volume.
+    ///
+    /// Specifies the master volume for the right output.
+    ///
+    /// # Note
+    ///
+    /// A value of 0 is treated as a volume of 1 (very quiet), and a value of 7
+    /// is treated as a volume of 8 (no volume reduction). Importantly, the
+    /// amplifier **never mutes** a non-silent input.
+    VolR = 0b0000_0111,
+}
+
+impl Bitmask<Nr50> for Byte {
+    fn test(&self, mask: Nr50) -> bool {
+        let mask = mask as Self;
+        self & mask != 0
+    }
+
+    fn set(&mut self, mask: Nr50, value: bool) {
+        let mask = mask as Self;
+        let wide = !Byte::from(value).wrapping_sub(1);
+        *self ^= (*self ^ wide) & mask;
+    }
+}
+
+impl From<Nr50> for Byte {
+    fn from(value: Nr50) -> Self {
+        value as _
     }
 }
