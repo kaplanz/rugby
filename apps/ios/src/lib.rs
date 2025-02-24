9bc6ebe8a12b7d38df1ca9e7b3af67a43369f9c3
Zakhary Kaplan <me@zakhary.dev>
2025-02-24 00:00:00 -0500

    feat(ios): forward log messages

--- apps/ios/src/lib.rs
+++ apps/ios/src/lib.rs
@@ -6,6 +6,22 @@ uniffi::setup_scaffolding!();
 
 mod api;
 mod err;
+mod log;
+
+use std::sync::Once;
 
 pub use api::*;
 pub use err::*;
+pub use log::*;
+
+/// Initialization singleton.
+static INIT: Once = Once::new();
+
+/// Initialize module.
+#[uniffi::export(name = "initialize")]
+pub fn init() {
+    INIT.call_once(|| {
+        // Initialize logging
+        log::init()
+    });
+}
