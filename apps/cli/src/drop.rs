08c3761fc6772d2fa1657dd433804ebec92c245e
Zakhary Kaplan <me@zakhary.dev>
2025-03-13 00:00:00 -0400

    refactor(cli): improve `ram::{load, dump}`

--- apps/cli/src/drop.rs
+++ apps/cli/src/drop.rs
@@ -9,7 +9,7 @@ pub fn emu(mut this: GameBoy, args: &Config) -> Result<()> {
     // Eject cartridge
     if let Some(cart) = this.eject() {
         // Save cart RAM
-        util::rom::dump(&args.emu.cart, &cart).context("error dumping save RAM")?;
+        util::ram::dump(&args.emu.cart, &cart).context("error dumping save RAM")?;
     }
 
     Ok(())
