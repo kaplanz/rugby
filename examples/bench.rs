829aa2335cb9f577379779b59eae1c7b95b21b00
Zakhary Kaplan <me@zakhary.dev>
2025-03-24 00:00:00 -0400

    feat(examples): use profiler in benchmark

--- examples/bench.rs
+++ examples/bench.rs
@@ -5,8 +5,18 @@ use std::time::{Duration, Instant};
 use rugby::arch::Block;
 use rugby::core::dmg::{Cartridge, FREQ, GameBoy};
 
+#[allow(unused)]
+#[path = "../apps/cli/src/app/run/emu/perf.rs"]
+mod perf;
+
+/// Sample cart ROM.
 const GAME: &[u8; 0x10000] = include_bytes!("../roms/games/porklike/porklike.gb");
 
+/// Application exit flag.
+///
+/// This value, `false` at initialization, will change to `true` exactly
+/// once during the lifetime of the program, signaling to all threads that
+/// they should exit.
 static EXIT: AtomicBool = AtomicBool::new(false);
 
 fn main() {
@@ -24,17 +34,26 @@ fn main() {
         EXIT.store(true, Ordering::Relaxed);
     });
 
+    // Initialize profiler
+    let mut prof = perf::Profiler::new();
+
     // Loop until exit
     while !EXIT.load(Ordering::Relaxed) {
         // Timestamp iteration start
         let instant = Instant::now();
+        // Perform emulation work
         for _ in 0..FREQ {
-            emu.cycle()
+            emu.cycle();
         }
         // Compute elapsed time
         let runtime = instant.elapsed();
         let speedup = runtime.as_secs_f64().recip();
         // Print iteration statistics
         println!("runtime: {runtime:>8.2?}, speedup: {speedup:>4.2}x");
+        // Update profiler
+        prof.tick_by(FREQ);
+        if let Some(freq) = prof.report_delay() {
+            println!("average: {:>4.2}x", freq / f64::from(FREQ));
+        }
     }
 }
