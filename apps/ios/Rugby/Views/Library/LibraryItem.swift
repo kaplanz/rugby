70b8b6764e3c5e7a828d247e683aefa9c7e0e5ef
Zakhary Kaplan <me@zakhary.dev>
2025-08-13 22:30:00 -0400

    feat(ios): add library animations

--- apps/ios/Rugby/Views/Library/LibraryItem.swift
+++ apps/ios/Rugby/Views/Library/LibraryItem.swift
@@ -42,7 +42,9 @@ struct LibraryItem<V: View>: View {
                     }
                     RenameButton()
                     Button("Duplicate", systemImage: "plus.square.on.square") {
-                        do { try lib.copy(game: game) } catch { err.or = error }
+                        withAnimation {
+                            do { try lib.copy(game: game) } catch { err.or = error }
+                        }
                     }
                 }
                 Section {
@@ -58,10 +60,10 @@ struct LibraryItem<V: View>: View {
                 Section {
                     !game.star
                         ? Button("Favourite", systemImage: "star") {
-                            game.star.toggle()
+                            withAnimation { game.star.toggle() }
                         }
                         : Button("Unfavourite", systemImage: "star.slash") {
-                            game.star.toggle()
+                            withAnimation { game.star.toggle() }
                         }
                 }
                 Section {
@@ -80,13 +82,17 @@ struct LibraryItem<V: View>: View {
                 TextField(game.name, text: $rename.text)
                 Button("Cancel", role: .cancel) {}
                 Button("Rename") {
-                    do { try lib.move(game: game, to: rename.text) } catch { err.or = error }
+                    withAnimation {
+                        do { try lib.move(game: game, to: rename.text) } catch { err.or = error }
+                    }
                 }
             }
             .alert("Are you sure you want to delete “\(game.name)”?", isPresented: $delete) {
                 Button("Cancel", role: .cancel) {}
                 Button("Delete", role: .destructive) {
-                    do { try lib.delete(game: game) } catch { err.or = error }
+                    withAnimation {
+                        do { try lib.delete(game: game) } catch { err.or = error }
+                    }
                 }
             } message: {
                 Text("This item will be moved to the trash.")
