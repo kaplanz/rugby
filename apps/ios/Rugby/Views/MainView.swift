65841c02977d6d2bf2f2e7c0e152a5184d1b9807
Zakhary Kaplan <me@zakhary.dev>
2025-09-01 17:00:00 -0400

    refactor(ios): replace `MainView.Subview` with `Bool`s

--- apps/ios/Rugby/Views/MainView.swift
+++ apps/ios/Rugby/Views/MainView.swift
@@ -11,40 +11,19 @@ import SwiftUI
 struct MainView: View {
     @Environment(Runtime.self) private var app
 
-    /// Welcome page.
+    /// Emulator page.
+    @State private var showEmulator = false
+    /// Settings page.
+    @State private var showSettings = false
+
+    /// Welcome data.
     @AppStorage("dev.zakhary.rugby.welcome") private var welcome: String?
-
-    /// Show welcome page.
+    /// Welcome page.
     private var showWelcome: Binding<Bool> {
-        Binding {
+        .init {
             welcome.flatMap(SemVer.Version.init).map { $0 < Build.VERSION } ?? true
-        } set: { show in
-            welcome = show ? nil : Build.VERSION.versionString()
-        }
-    }
-
-    /// Presented subview.
-    @State private var page: Subview?
-    private enum Subview {
-        case emulator
-        case settings
-    }
-
-    /// Show emulator subview.
-    private var showEmulator: Binding<Bool> {
-        Binding {
-            self.page == .emulator
         } set: { newValue in
-            self.page = newValue ? .emulator : nil
-        }
-    }
-
-    /// Show settings subview.
-    private var showSettings: Binding<Bool> {
-        Binding {
-            self.page == .settings
-        } set: { newValue in
-            self.page = newValue ? .settings : nil
+            welcome = newValue ? nil : Build.VERSION.versionString()
         }
     }
 
@@ -54,18 +33,23 @@ struct MainView: View {
                 .toolbar {
                     ToolbarItem {
                         Button("Settings", systemImage: "gearshape.fill") {
-                            showSettings.wrappedValue.toggle()
+                            showSettings.toggle()
                         }
                     }
                 }
         }
-        .sheet(isPresented: showSettings) {
+        .fullScreenCover(isPresented: $showEmulator) {
+            NavigationStack {
+                EmulatorView()
+            }
+        }
+        .sheet(isPresented: $showSettings) {
             NavigationStack {
                 SettingsView()
                     .toolbar {
                         ToolbarItem(placement: .confirmationAction) {
                             Button("Done", systemImage: "checkmark", role: .confirm) {
-                                showSettings.wrappedValue.toggle()
+                                showSettings.toggle()
                             }
                         }
                     }
@@ -74,13 +58,8 @@ struct MainView: View {
         .sheet(isPresented: showWelcome) {
             WelcomeView()
         }
-        .fullScreenCover(isPresented: showEmulator) {
-            NavigationStack {
-                EmulatorView()
-            }
-        }
         .onChange(of: app.game) { _, newValue in
-            showEmulator.wrappedValue = newValue != nil
+            showEmulator = newValue != nil
         }
     }
 }
