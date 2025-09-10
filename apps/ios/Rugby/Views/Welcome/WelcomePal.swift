6b13abae3dc25afee4573ee5bce0d9cabc2cee14
Zakhary Kaplan <me@zakhary.dev>
2025-09-09 23:00:00 -0400

    feat(ios): use palette picker in welcome

--- apps/ios/Rugby/Views/Welcome/WelcomePal.swift
+++ apps/ios/Rugby/Views/Welcome/WelcomePal.swift
@@ -9,43 +9,10 @@ import SwiftUI
 
 struct WelcomePal: View {
     @Environment(Options.self) private var opt
-    @Environment(\.dismiss) private var dismiss
-
-    @State private var showInfo = false
 
     var body: some View {
         @Bindable var cfg = opt.data
-
-        Form {
-            Picker(selection: $cfg.pal) {
-                ForEach(Palette.allCases) { pal in
-                    Label {
-                        Text(pal.description)
-                    } icon: {
-                        PaletteIcon(pal: pal)
-                    }
-                }
-            } label: {
-                HStack {
-                    Label("Palette", systemImage: "swatchpalette")
-                    Spacer()
-                    Button("Info", systemImage: "info.circle") {
-                        showInfo = true
-                    }
-                    .labelStyle(.iconOnly)
-                }
-                .confirmationDialog("Choose a palette", isPresented: $showInfo) {
-                } message: {
-                    Text(
-                        """
-                        Your palette selection will also be used as an accent for \
-                        within this app.
-                        """
-                    )
-                }
-            }
-            .pickerStyle(.inline)
-        }
+        PalettePicker(pal: $cfg.pal)
     }
 }
 
