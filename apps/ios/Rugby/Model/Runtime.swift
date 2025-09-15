fa393e1651eb38d7c29e5eecd01a54598d8b8c4b
Zakhary Kaplan <me@zakhary.dev>
2025-09-15 10:00:00 -0400

    feat(ios): upload emulator instance

--- apps/ios/Rugby/Model/Runtime.swift
+++ apps/ios/Rugby/Model/Runtime.swift
@@ -10,20 +10,29 @@ import RugbyKit
 
 @Observable
 final class Runtime {
-    /// Running game
-    private(set) var game: Game?
+    /// Emulator instance.
+    private(set) var emu: Emulator?
+
+    /// Emulator is active.
+    var active: Bool {
+        emu != nil
+    }
 
     /// Play a game.
     func play(_ game: Game) throws {
         // Ensure game is playable
         let _ = try Cartridge(data: game.data)
-        // Retain game to play
-        self.game = game
+        // Instantiate an emulator
+        emu = .init()
+        // Insert game to emulator
+        try emu?.play(game)
     }
 
     /// Stop playing.
-    func stop() {
-        // Remove game from play
-        game = nil
+    func stop() throws {
+        // Stop emulator
+        try emu?.stop()
+        // Drop instance
+        emu = nil
     }
 }
