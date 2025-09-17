f1789227996b6680cb9a176a09f4105bde032959
Zakhary Kaplan <me@zakhary.dev>
2025-09-17 10:00:00 -0400

    feat(ios): impl dummy sample rate setting

--- apps/ios/Rugby/Views/Settings/Section/AudioSettings.swift
+++ apps/ios/Rugby/Views/Settings/Section/AudioSettings.swift
@@ -14,7 +14,41 @@ struct AudioSettings: View {
         @Bindable var cfg = opt.data
 
         Form {
-            EmptyView()
+            Section {
+                Stepper(value: $cfg.aud.value, in: 8000...96000, step: 100) {
+                    Text(
+                        cfg.aud
+                            .converted(to: .kilohertz)
+                            .formatted(
+                                .measurement(
+                                    width: .abbreviated,
+                                    numberFormatStyle:
+                                        .number.precision(.fractionLength(1))
+                                ))
+                    )
+                }
+                Slider(
+                    value: $cfg.aud.value,
+                    in: 8000...96000,
+                    step: 100,
+                )
+                .onSubmit {
+                    cfg.aud.value.round()
+                }
+            } header: {
+                Label("Sample", systemImage: "dial.high")
+            } footer: {
+                Text(
+                    """
+                    Defines the sample rate to use for audio output.
+
+                    Must be in the range of 8 kHz to 96 kHz. Unless you have a \
+                    specific use case, there is no reason to change the \
+                    default value.
+                    """
+                )
+            }
+            .disabled(true)
         }
         .navigationTitle("Audio")
     }
