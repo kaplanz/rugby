6d5d65f66fa610568fcfe88f4275e450e392f32e
Zakhary Kaplan <me@zakhary.dev>
2025-09-17 11:00:00 -0400

    feat(ios): improve emulator startup sequence

--- apps/ios/Rugby/Model/Emulator/Emulator.swift
+++ apps/ios/Rugby/Model/Emulator/Emulator.swift
@@ -16,25 +16,44 @@ final class Emulator {
     /// Inserted game.
     private(set) var game: Game?
 
-    /// Plays a game.
-    func play(_ game: Game) throws {
-        // Initialize
+    /// Prepares system.
+    func prepare(_ game: Game) throws {
+        // Create cart
         let cart = try Cartridge(data: game.data)
         if let save = game.save {
             try cart.flash(save: save)
         }
         // Insert cart
-        core.play(cart)
+        core.insert(cart: cart)
         // Retain game
         self.game = game
     }
 
+    /// Starts emulation.
+    func start() {
+        self.core.start()
+    }
+
+    /// Plays a game.
+    ///
+    /// Prepares the system, then starts emulation.
+    func play(_ game: Game) throws {
+        // Prepare system
+        try self.prepare(game)
+        // Start emulator
+        self.start()
+    }
+
     /// Stops emulation.
     func stop() throws {
-        // Pause emulation
-        self.pause()
+        // Pause emulator
+        self.core.pause()
+        // Save screenshot
+        self.screenshot()
         // Eject cartridge
-        let cart = core.stop()
+        let cart = core.eject()
+        // Stop emulator
+        self.core.stop()
         // Update save RAM
         if cart?.header().board.power == true {
             game?.save = try cart?.dump()
@@ -43,15 +62,20 @@ final class Emulator {
 
     /// Pause emulation.
     func pause(_ state: Bool = true) {
-        // Take screenshot
-        game?.icon = core.video.image.map { UIImage(cgImage: $0) }
-        // Pause emulation
+        // Save screenshot
+        self.screenshot()
+        // Pause emulator
         (state ? core.pause : core.start)()
     }
 
     /// Reset emulator.
     func reset(_ kind: Reset) {
         core.reset(kind)
+    }
+
+    /// Save screenshot.
+    func screenshot() {
+        game?.icon = core.video.image.map { UIImage(cgImage: $0) }
     }
 
     /// Change speed.
