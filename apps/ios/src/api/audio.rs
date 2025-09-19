8eeac0299e7bab9366784aa13a45e99f50206a10
Zakhary Kaplan <me@zakhary.dev>
2025-09-18 23:30:00 -0400

    perf(ios): avoid uniffi bridge for audio samples
    Profiling the emulation look indicates that a significant amount of time
    is spent lifting audio samples from Rust to Swift via UniFFI. This
    indicates poor quality in the generated bridge code.
    
    To avoid relying on that code on the hot path, we can encode audio
    samples with a native type, in this instance `u64`, which is much
    cheaper to lift.
    
    Ideally in the future, this optimization will no longer be needed.

--- apps/ios/src/api/audio.rs
+++ apps/ios/src/api/audio.rs
@@ -12,8 +12,21 @@ impl GameBoy {
     /// Returns an audio sample with internal channels already mixed, ready for
     /// emulation.
     #[uniffi::method]
-    pub fn sample(&self) -> Sample {
-        self.inner.read().inside().audio().sample().mix()
+    pub fn sample(&self) -> u64 {
+        // Encode sample with a `u64`
+        //
+        // By encoding the sample's data within a `u64`, we can avoid UniFFI
+        // overhead. This should not be necessary, but it seems `Record` isn't
+        // very optimized.
+        //
+        // I understand this implementation may be considered a crime in some
+        // jurisdictions.
+        let Sample { lt, rt } = self.inner.read().inside().audio().sample().mix();
+        // Convert `f32`s to little-endian bytes
+        let lt = lt.to_le_bytes();
+        let rt = rt.to_le_bytes();
+        // Encode bytes in little-endian `u64`
+        u64::from_le_bytes([lt[0], lt[1], lt[2], lt[3], rt[0], rt[1], rt[2], rt[3]])
     }
 }
 
