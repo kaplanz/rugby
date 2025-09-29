ce37fc3ad7ff00229ce6f9a5c344fcd7cac7adf8
Zakhary Kaplan <me@zakhary.dev>
2025-09-29 15:00:00 -0400

    feat(site): initial impl

---
+++ site/src/markdown.js
@@ -0,0 +1,33 @@
+import { unified } from "unified";
+
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeStringify from "rehype-stringify";
+import rehypeSlug from "rehype-slug";
+
+// import remarkCallouts from "remark-callouts";
+import remarkGfm from "remark-gfm";
+// import remarkInlineFootnotes from "remark-inline-footnotes";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+
+export const markdown = (src) => unified()
+  .use(remarkParse, { fragment: true })
+  .use(remarkGfm)
+  // .use(remarkCallouts)
+  // .use(remarkInlineFootnotes)
+  .use(remarkRehype, { allowDangerousHtml: true })
+  .use(rehypeExternalLinks, {
+      target: "_blank",
+  })
+  .use(rehypeSlug)
+  .use(rehypeAutolinkHeadings, {
+    behavior: "wrap",
+    properties: {
+    className: "anchor",
+    rel: "nofollow",
+    },
+  })
+  .use(rehypeStringify, { allowDangerousHtml: true })
+  .processSync(src)
+  .toString();
