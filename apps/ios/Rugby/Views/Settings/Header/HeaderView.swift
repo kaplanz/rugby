14d72d27725568d8f022640dfad9b37dfffa59bc
Zakhary Kaplan <me@zakhary.dev>
2025-09-29 17:25:00 -0400

    feat(ios): improve header view

--- apps/ios/Rugby/Views/Settings/Header/HeaderView.swift
+++ apps/ios/Rugby/Views/Settings/Header/HeaderView.swift
@@ -9,8 +9,6 @@ import SwiftUI
 import WebKit
 
 struct HeaderView: View {
-    @State private var openLicense = false
-
     var body: some View {
         // Header
         Section {
@@ -40,39 +38,46 @@ struct HeaderView: View {
                 Label("Website", systemImage: "globe")
             }
             // License
-            Button {
-                openLicense.toggle()
+            NavigationLink {
+                List {
+                    Section {
+                        NavigationLink {
+                            TextFile(named: "LICENSE-MIT")
+                        } label: {
+                            Label("MIT", systemImage: "building.columns")
+                        }
+                        NavigationLink {
+                            TextFile(named: "LICENSE-APACHE")
+                        } label: {
+                            Label("Apache-2.0", systemImage: "bird")
+                        }
+                    } footer: {
+                        Text(
+                            """
+                            This project is dual-licensed under both MIT License
+                            and Apache License 2.0. You have permission to use
+                            this code under the conditions of either license
+                            pursuant to the rights granted by the chosen
+                            license.
+                            """
+                        )
+                    }
+                }
+                .navigationTitle("License")
             } label: {
                 Label("License", systemImage: "doc.text")
-                    .foregroundStyle(.tint)
-            }
-            .confirmationDialog("Choose License", isPresented: $openLicense) {
-                NavigationLink {
-                    TextFile(named: "LICENSE-MIT")
-                } label: {
-                    Label("MIT", systemImage: "building.columns")
-                        .foregroundStyle(.tint)
-                }
-                NavigationLink {
-                    TextFile(named: "LICENSE-APACHE")
-                } label: {
-                    Label("Apache-2.0", systemImage: "bird")
-                        .foregroundStyle(.tint)
-                }
             }
             // Privacy
             NavigationLink {
                 TextFile(named: "PRIVACY.md", kind: .markdown)
             } label: {
                 Label("Privacy", systemImage: "hand.raised")
-                    .foregroundStyle(.tint)
             }
             // Credits
             NavigationLink {
                 TextFile(named: "CREDITS.md", kind: .markdown)
             } label: {
-                Label("Credits", systemImage: "person.2.shield")
-                    .foregroundStyle(.tint)
+                Label("Credits", systemImage: "person.bust")
             }
         } footer: {
             Text("Copyright &copy; \(Build.DATE.formatted(.dateTime.year())) Zakhary Kaplan")
