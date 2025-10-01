c2d7d1c3e934a6497364adb23147bee428bdb605
Zakhary Kaplan <me@zakhary.dev>
2025-10-01 17:00:00 -0400

    feat(ios): use build datetime from obj-c

---
+++ apps/ios/Rugby/Build/Build.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const __OBJC_DATE__;
+extern NSString * const __OBJC_TIME__;
