a0c2c23dd8c6b78c57d5165b06d68c14b2b184b3
Zakhary Kaplan <me@zakhary.dev>
2025-10-01 17:00:00 -0400

    feat(ios): use build datetime from obj-c

---
+++ apps/ios/Rugby/Build/Build.m
@@ -0,0 +1,13 @@
+//
+//  Build.m
+//  Rugby
+//
+//  Created by Zakhary Kaplan on 2025-10-01.
+//
+
+#import "Build.h"
+
+#import <Foundation/Foundation.h>
+
+NSString * const __OBJC_DATE__ = @__DATE__;
+NSString * const __OBJC_TIME__ = @__TIME__;
