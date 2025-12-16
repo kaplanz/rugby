5715dd1ef31309047b3642cdfe48b95472743b03
Zakhary Kaplan <me@zakhary.dev>
2025-12-15 23:30:00 -0500

    build(swift): bump platform versions

--- bind/swift/pkg/Package.swift
+++ bind/swift/pkg/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 //
 // The swift-tools-version declares the minimum version of Swift required to
 // build this package.
@@ -8,8 +8,8 @@ import PackageDescription
 let package = Package(
     name: "rugby",
     platforms: [
-        .iOS(.v18),
-        .macOS(.v15),
+        .iOS(.v26),
+        .macOS(.v26),
     ],
     products: [
         // Products define the executables and libraries a package produces,
