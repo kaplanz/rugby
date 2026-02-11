c01c816994126564b0e749233b52b01a88cb1efa
Zakhary Kaplan <me@zakhary.dev>
2026-02-10 23:00:00 -0500

    feat: derive `facet::Facet`

--- extras/cfg/src/lib.rs
+++ extras/cfg/src/lib.rs
@@ -15,9 +15,18 @@ pub mod opt;
 #[derive(Debug, Default, Merge)]
 #[cfg_attr(feature = "clap", derive(clap::Args))]
 #[cfg_attr(
+    feature = "facet",
+    derive(facet::Facet),
+    facet(default, deny_unknown_fields)
+)]
+#[cfg_attr(
     feature = "serde",
     derive(serde::Deserialize),
     serde(default, deny_unknown_fields)
+)]
+#[cfg_attr(
+    all(feature = "facet", feature = "serde"),
+    expect(clippy::unsafe_derive_deserialize)
 )]
 pub struct Config {
     /// Frontend options.
