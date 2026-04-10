0f24ce3dd6253fc2eb7eb757a1a3227c9df3ca15
Zakhary Kaplan <me@zakhary.dev>
2026-04-10 00:00:00 -0400

    fix(ppu): implement edge-triggered STAT interrupts
    Mode execution should complete before the STAT check so the next mode is
    used when evaluating interrupt sources, fixing the timing of when
    interrupts occur.
    
    Tracking the previous interrupt signal level (via added `int` flag) is
    necessary to ensure STAT interrupts triggers only on a rising edge
    transition, preventing duplicate interrupts for a single trigger event.

--- core/src/parts/ppu/exec/mod.rs
+++ core/src/parts/ppu/exec/mod.rs
@@ -47,42 +47,59 @@ impl Mode {
 
     #[must_use]
     pub(super) fn exec(self, ppu: &mut Ppu) -> Self {
-        // Update status register
+        // Execute state machine
+        let next = match self {
+            // Mode 2
+            Mode::Scan(scan) => scan.exec(ppu),
+            // Mode 3
+            Mode::Draw(draw) => draw.exec(ppu),
+            // Mode 0
+            Mode::HBlank(hblank) => hblank.exec(ppu),
+            // Mode 1
+            Mode::VBlank(vblank) => vblank.exec(ppu),
+        };
+
+        // Compute STAT register
         let ly = ppu.reg.ly.load();
         let lyc = ppu.reg.lyc.load();
         let stat = {
             let mut stat = ppu.reg.stat.load();
-            stat ^= (0x03 & stat) ^ self.value();
+            // Mode bits [1:0]
+            stat ^= (0x03 & stat) ^ next.value();
+            // LYC=LY bit [2]
             stat ^= (0x04 & stat) ^ (u8::from(ly == lyc) << 2);
             stat
         };
+        // Update STAT register
         ppu.reg.stat.store(stat);
 
-        // Trigger interrupts
-        if ppu.etc.dot == 0 {
-            let mut int = 0;
-            // LYC=LY
-            int |= u8::from(lyc == ly) << 6;
-            // Mode 2
-            int |= u8::from(matches!(self, Mode::Scan(_))) << 5;
-            // Mode 1
-            int |= u8::from(matches!(self, Mode::VBlank(_))) << 4;
-            // Mode 0
-            int |= u8::from(matches!(self, Mode::HBlank(_))) << 3;
-            // Check for interrupts
-            if int & (stat & 0x78) != 0 {
-                // Request an interrupt
-                ppu.int.raise(Interrupt::LcdStat);
-            }
-        }
+        // Compute STAT interrupt
+        let int = {
+            // Mode bits [5:3]
+            #[rustfmt::skip]
+            let mode = match next {
+                // Mode 0
+                Mode::HBlank(_) => 1 << 3,
+                // Mode 1
+                Mode::VBlank(_) => 1 << 4,
+                // Mode 3
+                Mode::Draw(_)   => 0,
+                // Mode 2
+                Mode::Scan(_)   => 1 << 5,
+            };
+            // LYC=LY bit [6]
+            let line = u8::from(lyc == ly) << 6;
 
-        // Execute the current mode
-        let next = match self {
-            Mode::Scan(scan) => scan.exec(ppu),
-            Mode::Draw(draw) => draw.exec(ppu),
-            Mode::HBlank(hblank) => hblank.exec(ppu),
-            Mode::VBlank(vblank) => vblank.exec(ppu),
+            // Combine activation sources
+            ((mode | line) & stat) != 0
         };
+        // Trigger STAT interrupt
+        if int && !ppu.etc.int {
+            // Only trigger on rising edge
+            ppu.int.raise(Interrupt::LcdStat);
+        }
+        // Update STAT interrupt
+        ppu.etc.int = int;
 
         // Increment dot count
         ppu.etc.dot += 1;
