45d4a2389dc318f8f7f60871135f8d8709f3b731
Zakhary Kaplan <me@zakhary.dev>
2026-04-29 19:00:00 -0400

    feat(dmg): support revision-specific boot sequence

--- core/src/model/dmg/mod.rs
+++ core/src/model/dmg/mod.rs
@@ -52,15 +52,66 @@ pub struct GameBoy<R: Revision = rev::C> {
     _rev: PhantomData<R>,
 }
 
+/// Revision-specific hardware model.
+trait Instance {
+    /// Simulate the bootup sequence.
+    fn boot(&mut self);
+}
+
+#[rustfmt::skip]
+impl Instance for GameBoy<rev::Zero> {
+    fn boot(&mut self) {
+        let cpu = &mut self.main.soc.cpu;
+
+        // Initialize registers
+        #[expect(clippy::items_after_statements)]
+        type Select = <Cpu as Port<u16>>::Select;
+        cpu.store(Select::AF, 0x0100_u16);
+        cpu.store(Select::BC, 0xff13_u16);
+        cpu.store(Select::DE, 0x00c1_u16);
+        cpu.store(Select::HL, 0x8403_u16);
+        cpu.store(Select::SP, 0xfffe_u16);
+
+        // Perform bootup sequence
+        cpu.write(0xff40, 0x91); // enable display
+        cpu.write(0xff50, 0x01); // disable boot ROM
+        cpu.exec(0xfb);          // enable interrupts
+        cpu.goto(0x0100);        // transfer program control
+    }
+}
+
+#[rustfmt::skip]
+impl Instance for GameBoy<rev::A> {
+    fn boot(&mut self) {
+        let cpu = &mut self.main.soc.cpu;
+
+        // Initialize registers
+        #[expect(clippy::items_after_statements)]
+        type Select = <Cpu as Port<u16>>::Select;
+        cpu.store(Select::AF, 0x01b0_u16);
+        cpu.store(Select::BC, 0x0013_u16);
+        cpu.store(Select::DE, 0x00d8_u16);
+        cpu.store(Select::HL, 0x014d_u16);
+        cpu.store(Select::SP, 0xfffe_u16);
+
+        // Perform bootup sequence
+        cpu.write(0xff40, 0x91); // enable display
+        cpu.write(0xff50, 0x01); // disable boot ROM
+        cpu.exec(0xfb);          // enable interrupts
+        cpu.goto(0x0100);        // transfer program control
+    }
+}
+
 impl<R: Revision> GameBoy<R> {
     /// Constructs a new `GameBoy`.
     #[must_use]
-    pub fn new() -> Self {
+    #[expect(private_bounds)]
+    pub fn new() -> Self
+    where
+        Self: Instance,
+    {
         let mut this = Self::default();
-
-        // Simulate bootup sequence
         this.boot();
-
         this
     }
 
@@ -82,24 +133,12 @@ impl<R: Revision> GameBoy<R> {
     /// This prepares the `GameBoy` to run the contents of a game cartridge.
     /// When no [boot ROM](boot::Boot) is installed, this must be called before
     /// cartridge execution.
-    #[rustfmt::skip]
-    pub fn boot(&mut self) {
-        let cpu = &mut self.main.soc.cpu;
-
-        // Initialize registers
-        #[expect(clippy::items_after_statements)]
-        type Select = <Cpu as Port<u16>>::Select;
-        cpu.store(Select::AF, 0x01b0_u16);
-        cpu.store(Select::BC, 0x0013_u16);
-        cpu.store(Select::DE, 0x00d8_u16);
-        cpu.store(Select::HL, 0x014d_u16);
-        cpu.store(Select::SP, 0xfffe_u16);
-
-        // Perform bootup sequence
-        cpu.write(0xff40, 0x91); // enable display
-        cpu.write(0xff50, 0x01); // disable boot ROM
-        cpu.exec(0xfb);          // enable interrupts
-        cpu.goto(0x0100);        // transfer program control
+    #[expect(private_bounds)]
+    pub fn boot(&mut self)
+    where
+        Self: Instance,
+    {
+        <Self as Instance>::boot(self);
     }
 
     /// Gets the inserted game cartridge, if any.
@@ -135,7 +174,10 @@ impl<R: Revision> GameBoy<R> {
     }
 }
 
-impl<R: Revision> Block for GameBoy<R> {
+impl<R: Revision> Block for GameBoy<R>
+where
+    GameBoy<R>: Instance,
+{
     fn ready(&self) -> bool {
         self.main.ready()
     }
@@ -152,7 +194,7 @@ impl<R: Revision> Block for GameBoy<R> {
     }
 }
 
-impl<R: Revision> Core for GameBoy<R> {}
+impl<R: Revision> Core for GameBoy<R> where GameBoy<R>: Instance {}
 
 impl<R: Revision> core::has::Audio for GameBoy<R> {
     type Audio = Apu;
