59846379146ef46cda4c3d33549649c88aaf3e83
Zakhary Kaplan <me@zakhary.dev>
2026-04-28 20:00:00 -0400

    refactor(joypad): re-impl registers

--- core/src/parts/joypad.rs
+++ core/src/parts/joypad.rs
@@ -1,7 +1,7 @@
 //! Player input device.
 
 use std::collections::HashSet;
-use std::ops::{BitOr, Not};
+use std::ops::BitOr;
 
 use log::{debug, trace};
 use rugby_arch::mem::Memory;
@@ -72,18 +72,6 @@ impl Mode {
 
     fn select(self, btn: Button) -> bool {
         btn.mode() & self.mode() != 0
-    }
-}
-
-impl From<u8> for Mode {
-    fn from(value: u8) -> Self {
-        match value & Self::MASK {
-            0b0000_0000 => Mode::None,
-            0b0001_0000 => Mode::DPad,
-            0b0010_0000 => Mode::Keys,
-            0b0011_0000 => Mode::Both,
-            _ => unreachable!(),
-        }
     }
 }
 
@@ -170,7 +158,8 @@ impl Register for Control {
     type Value = u8;
 
     fn load(&self) -> Self::Value {
-        self.keys
+        !self
+            .keys
             .iter()
             .copied()
             // keep only selected keys
@@ -179,14 +168,74 @@ impl Register for Control {
             .map(Button::key)
             // apply selected mode
             .fold(self.mode.mode(), BitOr::bitor)
-            // values are read inverted
-            .not()
     }
 
     fn store(&mut self, value: Self::Value) {
-        self.mode = Mode::from(
-            // values are written inverted
-            !value,
-        );
+        let p1 = reg::P1::from_bits(!value);
+        self.mode = match (p1.action(), p1.direction()) {
+            (false, false) => Mode::None,
+            (false, true) => Mode::DPad,
+            (true, false) => Mode::Keys,
+            (true, true) => Mode::Both,
+        };
+    }
+}
+
+pub mod reg {
+    use bitfield_struct::bitfield;
+    use rugby_arch::mem::Memory;
+    use rugby_arch::reg::Register;
+
+    /// `P1`: Joypad register.
+    ///
+    /// | Bit | Name                     | Use |
+    /// |-----|--------------------------|-----|
+    /// | 7-6 | Unused.                  | -   |
+    /// |   5 | Action button select.    | W   |
+    /// |   4 | Direction button select. | W   |
+    /// | 3-0 | Button states.           | R   |
+    ///
+    /// See more details [here][p1].
+    ///
+    /// [p1]: https://gbdev.io/pandocs/Joypad_Input.html
+    #[bitfield(u8, order = msb)]
+    #[derive(PartialEq, Eq)]
+    pub struct P1 {
+        /// `P1[7:6]`: Unused.
+        #[bits(2)]
+        __: u8,
+        /// `P1[5]`: Action button select.
+        #[bits(1)]
+        pub action: bool,
+        /// `P1[4]`: Direction button select.
+        #[bits(1)]
+        pub direction: bool,
+        /// `P1[3:0]`: Button states.
+        #[bits(4)]
+        pub keys: u8,
+    }
+
+    impl Memory for P1 {
+        fn read(&self, _: u16) -> rugby_arch::mem::Result<u8> {
+            Ok(self.load())
+        }
+
+        fn write(&mut self, _: u16, data: u8) -> rugby_arch::mem::Result<()> {
+            self.store(data);
+            Ok(())
+        }
+    }
+
+    impl Register for P1 {
+        type Value = u8;
+
+        fn load(&self) -> Self::Value {
+            // unused bits 7:6 always read as 1
+            self.into_bits() | 0xc0
+        }
+
+        fn store(&mut self, value: Self::Value) {
+            *self = Self::from_bits(value);
+        }
     }
 }
