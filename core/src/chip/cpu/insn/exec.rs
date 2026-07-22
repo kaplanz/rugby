e93b082d32345ebb8c4b41289d36d1a1f9b24926
Zakhary Kaplan <me@zakhary.dev>
2026-07-21 23:00:00 -0400

    feat(cpu): migrate `ret`

--- core/src/chip/cpu/insn/exec.rs
+++ core/src/chip/cpu/insn/exec.rs
@@ -30,7 +30,6 @@ pub(super) type Start = fn() -> Operation;
 /// Instruction operation state.
 #[derive(Clone, Debug)]
 pub enum Operation {
-    Ret(ret::Ret),
     Reti(reti::Reti),
     Rl(rl::Rl),
     Rla(rla::Rla),
@@ -58,7 +57,6 @@ impl Execute for Operation {
     #[rustfmt::skip]
     fn exec(self, code: u8, cpu: &mut Cpu) -> Result<Option<Operation>> {
         match self {
-            Operation::Ret(inner)    => inner.exec(code, cpu),
             Operation::Reti(inner)   => inner.exec(code, cpu),
             Operation::Rl(inner)     => inner.exec(code, cpu),
             Operation::Rla(inner)    => inner.exec(code, cpu),
