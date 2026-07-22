93e1483f4e48b180c8dbc1e65a7bf23cef766538
Zakhary Kaplan <me@zakhary.dev>
2026-07-21 23:00:00 -0400

    feat(cpu): migrate `decw`

--- core/src/chip/cpu/insn/exec/decw.rs
+++ core/src/chip/cpu/insn/exec/decw.rs
@@ -1,65 +1,38 @@
 use rugby_arch::reg::Register;
 
-use super::{Cpu, Error, Execute, Operation, Return};
+use super::{Cpu, Exec, Instruction};
 
-pub const fn default() -> Operation {
-    Operation::Decw(Decw::Fetch)
+pub const fn default() -> Exec {
+    cycle2
 }
 
-#[derive(Clone, Debug, Default)]
-pub enum Decw {
-    #[default]
-    Fetch,
-    Execute(u16),
-}
-
-impl Execute for Decw {
-    #[rustfmt::skip]
-    fn exec(self, code: u8, cpu: &mut Cpu) -> Return {
-        match self {
-            Self::Fetch        => fetch(code, cpu),
-            Self::Execute(op1) => execute(code, cpu, op1),
-        }
-    }
-}
-
-impl From<Decw> for Operation {
-    fn from(value: Decw) -> Self {
-        Self::Decw(value)
-    }
-}
-
-fn fetch(code: u8, cpu: &mut Cpu) -> Return {
-    // Check opcode
+fn cycle2(code: u8, cpu: &mut Cpu) -> Option<Instruction> {
+    // Load r16
     let op1 = match code {
         0x0b => cpu.reg.bc().load(),
         0x1b => cpu.reg.de().load(),
         0x2b => cpu.reg.hl().load(),
         0x3b => cpu.reg.sp.load(),
-        code => return Err(Error::Opcode(code)),
+        code => unreachable!("unexpected opcode: {code:#04X}"),
     };
 
-    // Proceed
-    Ok(Some(Decw::Execute(op1).into()))
-}
-
-fn execute(code: u8, cpu: &mut Cpu, op1: u16) -> Return {
     // Execute DECW
-    let res = op1.wrapping_sub(1);
+    let res = cpu.blk.idu.dec(op1);
     match code {
-        0x0b => {
-            cpu.reg.bc_mut().store(res);
-        }
-        0x1b => {
-            cpu.reg.de_mut().store(res);
-        }
-        0x2b => {
-            cpu.reg.hl_mut().store(res);
-        }
+        0x0b => cpu.reg.bc_mut().store(res),
+        0x1b => cpu.reg.de_mut().store(res),
+        0x2b => cpu.reg.hl_mut().store(res),
         0x3b => cpu.reg.sp.store(res),
-        code => return Err(Error::Opcode(code)),
+        code => unreachable!("unexpected opcode: {code:#04X}"),
     }
 
+    // Proceed
+    cpu.step(cycle3)
+}
+
+fn cycle3(_: u8, _: &mut Cpu) -> Option<Instruction> {
+    // Delay by 1 cycle
+
     // Finish
-    Ok(None)
+    None
 }
