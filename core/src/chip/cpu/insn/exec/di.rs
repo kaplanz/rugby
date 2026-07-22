108aa864f6aba8d796303518fdf5621f0c58a384
Zakhary Kaplan <me@zakhary.dev>
2026-07-21 23:00:00 -0400

    feat(cpu): migrate `di`

--- core/src/chip/cpu/insn/exec/di.rs
+++ core/src/chip/cpu/insn/exec/di.rs
@@ -1,39 +1,18 @@
-use super::{Cpu, Error, Execute, Ime, Operation, Return};
+use super::{Cpu, Exec, Ime, Instruction};
 
-pub const fn default() -> Operation {
-    Operation::Di(Di::Execute)
+pub const fn default() -> Exec {
+    cycle2
 }
 
-#[derive(Clone, Debug, Default)]
-pub enum Di {
-    #[default]
-    Execute,
-}
-
-impl Execute for Di {
-    #[rustfmt::skip]
-    fn exec(self, code: u8, cpu: &mut Cpu) -> Return {
-        match self {
-            Self::Execute => execute(code, cpu),
-        }
-    }
-}
-
-impl From<Di> for Operation {
-    fn from(value: Di) -> Self {
-        Self::Di(value)
-    }
-}
-
-fn execute(code: u8, cpu: &mut Cpu) -> Return {
+fn cycle2(code: u8, cpu: &mut Cpu) -> Option<Instruction> {
     // Check opcode
     if code != 0xf3 {
-        return Err(Error::Opcode(code));
+        unreachable!("unexpected opcode: {code:#04X}");
     }
 
     // Execute DI
     cpu.etc.ime = Ime::Disabled;
 
     // Finish
-    Ok(None)
+    None
 }
