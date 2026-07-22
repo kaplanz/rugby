aa1768c45af8901e3dbeffae708286e0e19e153a
Zakhary Kaplan <me@zakhary.dev>
2026-07-21 23:00:00 -0400

    feat(cpu): migrate `call`

--- core/src/chip/cpu/insn/exec.rs
+++ core/src/chip/cpu/insn/exec.rs
@@ -30,7 +30,6 @@ pub(super) type Start = fn() -> Operation;
 /// Instruction operation state.
 #[derive(Clone, Debug)]
 pub enum Operation {
-    Call(call::Call),
     Ccf(ccf::Ccf),
     Cp(cp::Cp),
     Cpl(cpl::Cpl),
@@ -81,7 +80,6 @@ impl Execute for Operation {
     #[rustfmt::skip]
     fn exec(self, code: u8, cpu: &mut Cpu) -> Result<Option<Operation>> {
         match self {
-            Operation::Call(inner)   => inner.exec(code, cpu),
             Operation::Ccf(inner)    => inner.exec(code, cpu),
             Operation::Cp(inner)     => inner.exec(code, cpu),
             Operation::Cpl(inner)    => inner.exec(code, cpu),
