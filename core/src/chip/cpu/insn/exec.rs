b6cb543e5125b6ed1d9748e3292f62f11dbd50f2
Zakhary Kaplan <me@zakhary.dev>
2026-07-21 23:00:00 -0400

    feat(cpu): migrate `addw`

--- core/src/chip/cpu/insn/exec.rs
+++ core/src/chip/cpu/insn/exec.rs
@@ -30,7 +30,6 @@ pub(super) type Start = fn() -> Operation;
 /// Instruction operation state.
 #[derive(Clone, Debug)]
 pub enum Operation {
-    Addw(addw::Addw),
     And(and::And),
     Bit(bit::Bit),
     Call(call::Call),
@@ -84,7 +83,6 @@ impl Execute for Operation {
     #[rustfmt::skip]
     fn exec(self, code: u8, cpu: &mut Cpu) -> Result<Option<Operation>> {
         match self {
-            Operation::Addw(inner)   => inner.exec(code, cpu),
             Operation::And(inner)    => inner.exec(code, cpu),
             Operation::Bit(inner)    => inner.exec(code, cpu),
             Operation::Call(inner)   => inner.exec(code, cpu),
